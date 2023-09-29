88e7a78254affe11192b931889642a23b2baf864
澄三彩 <634725355@qq.com>
2023-09-30 00:49:47 +0800

    全局边框完成

--- src/app/components/home/LineBorder/index.tsx
+++ src/app/components/home/LineBorder/index.tsx
@@ -6,10 +6,15 @@ import borderBottom from "img/border_bottom.png";
 import './index.scss'
 
 export default function LineBorder() {
-
     return (
-        <div className='absolute w-full h-full' >
+        <div className='absolute w-full h-full opacity-20' >
             <div className='absolute left-5 top-20 bg-line-tlf bg-size-100 h-28 w-1' ></div>
+            <div className='absolute left-5 bottom-5 ba-line-brl bg-size-100 h-32 w-32' ></div>
+            <div className='absolute left-5 top-1/2 -translate-y-1/2 bg-repeat bg-line-lr h-36 w-1' ></div>
+            <div className='absolute right-5 top-20 rotate-y-180 bg-line-tlf bg-size-100 h-28 w-1' ></div>
+            <div className='absolute right-5 bottom-5 rotate-y-180 ba-line-brl bg-size-100 h-32 w-32' ></div>
+            <div className='absolute right-5 top-1/2 -translate-y-1/2 bg-repeat bg-line-lr h-36 w-1' ></div>
+            <div className='absolute bottom-5 bg-line-bottom left-1/2 -translate-x-1/2 bg-repeat w-1/2 h-1' ></div>
         </div>
     )
 }
