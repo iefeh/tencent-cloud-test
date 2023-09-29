89ddf7e2a4e2e025bdd445a3110c3ced5f2fa584
Eki <ichinoseeki@outlook.com>
2023-09-29 18:23:06 +0800

    feat: 添加首页视频蒙版，调整基本按钮样式

--- src/app/components/home/IndexSlide.tsx
+++ src/app/components/home/IndexSlide.tsx
@@ -3,13 +3,17 @@ import PageDesc from "../common/PageDesc";
 export default function IndexSlide() {
   return (
     <div className="bg-video w-full h-screen relative flex justify-center items-center">
-      <video
-        className="w-full h-full object-cover absolute inset-100"
-        src="/video/ntfbg.webm"
-        autoPlay
-        muted
-        loop
-      ></video>
+      <div className="video-container absolute inset-0">
+        <video
+          className="w-full h-full object-cover"
+          src="/video/ntfbg.webm"
+          autoPlay
+          muted
+          loop
+        ></video>
+
+        <div className="video-mask absolute inset-0 z-10 bg-black/70"></div>
+      </div>
 
       <PageDesc
         title="800 Exclusive<br>Destiny TETRA NFTs Awaits"
