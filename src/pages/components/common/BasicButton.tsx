4e947ac1cba6fd54ef2b1fe95bf88d8053750675
duanleiyu <ichinoseeki@outlook.com>
2023-10-07 23:35:24 +0800

    fix: 修改按钮点击错误

--- src/pages/components/common/BasicButton.tsx
+++ src/pages/components/common/BasicButton.tsx
@@ -19,9 +19,10 @@ export default function BasicButton(props: Props) {
 
   return (
     <button
-      className={"basic-button uppercase text-sm px-6 py-1 border border-solid rounded-3xl text-white hover:border-basic-yellow hover:text-basic-yellow hover:shadow-basic-yellow hover:shadow-[0_0_0.375rem_#F6C799] transition-all duration-500 delay-75"}
-      {...(props.link ? { onClick: onLinkClick } : {})}
-      onClick={props.onClick}
+      className={
+        "basic-button uppercase text-sm px-6 py-1 border border-solid rounded-3xl text-white hover:border-basic-yellow hover:text-basic-yellow hover:shadow-basic-yellow hover:shadow-[0_0_0.375rem_#F6C799] transition-all duration-500 delay-75"
+      }
+      onClick={props.onClick || (props.link && onLinkClick) || undefined}
     >
       {props.label}
     </button>
