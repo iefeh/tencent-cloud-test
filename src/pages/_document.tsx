1380cde3fc3cbdad2e74918beffea7f34cacd83b
Eki <ichinoseeki@outlook.com>
2023-10-09 15:08:39 +0800

    feat: 添加高分屏视频禁止缩放配置

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head title="Moonveil">
         <meta name="description" content="Moonveil Official Website" />
+        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
         <script src="https://accounts.google.com/gsi/client" async defer></script>
         <script type="text/javascript" async src="https://platform.twitter.com/widgets.js"></script>
       </Head>
