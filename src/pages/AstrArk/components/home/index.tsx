17c840c944e90cdee9f0e149cc8357a91da238f8
Eki <ichinoseeki@outlook.com>
2023-10-10 17:58:18 +0800

    feat: astrark替换smoothscroll

--- src/pages/AstrArk/components/home/index.tsx
+++ src/pages/AstrArk/components/home/index.tsx
@@ -1,88 +1,67 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import arrowImg from "img/astrark/arrow.png";
-import Image from "next/image";
+import React, { MutableRefObject, useEffect, useRef, useState } from 'react';
+import arrowImg from 'img/astrark/arrow.png';
+import Image from 'next/image';
+import { Scrollbar } from 'smooth-scrollbar/scrollbar';
 
 interface Props {
-  toDisable: () => any;
-  toEnable: () => any;
+  sbRef: MutableRefObject<Scrollbar | undefined>;
+  onScrollStatusChange: (enable: boolean) => void;
 }
 
 const AstrarkHome: React.FC<Props> = (props) => {
-  const { toDisable, toEnable } = props;
-  const [showFullVideo, setShowFullVideo] = useState(false);
+  const { sbRef, onScrollStatusChange } = props;
   const textDomRef = useRef<HTMLDivElement>(null);
-  const maxScale = 6;
-  const maxAutoScale = 20;
-  const scaleAniDuration = 300;
-  const autoAniDuration = 500;
+  const maxScale = 30;
+  const scaleAniDuration = 1000;
   const fpsSec = 16.6667;
   let currenScale = 1;
   let targetScale = 1;
   let currenOpacity = 1;
-  let targetOpacity = 0;
   let scalePf = 0;
   let opacityPf = 0;
 
-  useEffect(() => {
-    toDisable();
-  }, []);
-
-  const runAutoAni = () => {
-    if (!textDomRef.current) return;
-    if (currenScale >= maxAutoScale) {
-      setShowFullVideo(true);
-      window.removeEventListener("wheel", scrollControl);
-      toEnable();
-      return;
-    }
-
-    currenScale += scalePf;
-    currenOpacity += opacityPf;
-    textDomRef.current.style.transform = `scale(${currenScale})`;
-    textDomRef.current.style.opacity = currenOpacity + "";
-    requestAnimationFrame(runAutoAni);
-  };
-
   const runScaleAni = () => {
     if (!textDomRef.current) return;
+    if (currenScale === targetScale) return;
 
-    if (Math.abs(currenScale - targetScale) < 0.05) {
+    if (Math.abs(currenScale - targetScale) < 0.02) {
       currenScale = targetScale;
-      textDomRef.current.style.transform = `scale(${currenScale})`;
-      if (currenScale >= maxScale) {
-        targetScale = maxAutoScale;
-        scalePf = ((targetScale - currenScale) * fpsSec) / autoAniDuration;
-        opacityPf =
-          ((targetOpacity - currenOpacity) * fpsSec) / autoAniDuration;
-        currenOpacity = 1;
-        targetOpacity = 0;
-        runAutoAni();
+      if (targetScale === maxScale) {
+        onScrollStatusChange(true);
+      } else {
+        onScrollStatusChange(false);
       }
-      return;
+    } else {
+      currenScale += scalePf;
+      currenOpacity += opacityPf;
     }
 
-    currenScale += scalePf;
     textDomRef.current.style.transform = `scale(${currenScale})`;
+    textDomRef.current.style.opacity = currenOpacity + '';
     requestAnimationFrame(runScaleAni);
   };
 
   const scrollControl = (e: WheelEvent) => {
     if (e.deltaY === 0) return;
 
+    if (currenScale < maxScale) {
+      e.stopPropagation();
+    }
+
     const scaleDiff = (e.deltaY / scaleAniDuration) * maxScale;
-    targetScale = Math.max(1, Math.min(maxScale, targetScale + scaleDiff));
+    const nextTargetScale = Math.max(1, Math.min(maxScale, targetScale + scaleDiff));
+    if (targetScale === nextTargetScale) return;
+
+    targetScale = nextTargetScale;
     scalePf = ((targetScale - currenScale) * fpsSec) / scaleAniDuration;
     runScaleAni();
   };
 
-  useEffect(() => {
-    window.addEventListener("wheel", scrollControl);
-
-    return () => window.removeEventListener("wheel", scrollControl);
-  }, []);
-
   return (
-    <div className="astrark-home relative w-full h-screen flex justify-center items-center">
+    <div
+      className="astrark-home relative w-full h-screen flex justify-center items-center overflow-hidden"
+      onWheel={(e) => scrollControl(e as any)}
+    >
       <video
         className="object-cover absolute left-0 top-0 w-full h-full z-0"
         autoPlay
@@ -94,14 +73,12 @@ const AstrarkHome: React.FC<Props> = (props) => {
         <source src="/video/astrark.mp4" />
       </video>
 
-      {showFullVideo || (
-        <div
-          ref={textDomRef}
-          className="text uppercase text-[18rem] font-semakin absolute left-0 top-0 flex justify-center items-center w-full h-full bg-black text-white text-center mix-blend-multiply z-10 origin-center"
-        >
-          AstrArk
-        </div>
-      )}
+      <div
+        ref={textDomRef}
+        className="text uppercase text-[18rem] font-semakin absolute left-0 top-0 flex justify-center items-center w-full h-full bg-black text-white text-center mix-blend-multiply z-10 origin-center"
+      >
+        AstrArk
+      </div>
 
       <Image
         className="w-[3.1875rem] h-[1.75rem] absolute left-1/2 -translate-x-1/2 bottom-[4.5625rem] z-20"
