953e1d4f2c32f3e2a042183fbb1171b11fce9cba
duanleiyu <ichinoseeki@outlook.com>
2023-10-11 21:48:24 +0800

    feat: 修改astrark第二屏文字

--- src/pages/AstrArk/components/secondDesc/index.tsx
+++ src/pages/AstrArk/components/secondDesc/index.tsx
@@ -14,7 +14,7 @@ export default function WorldView() {
         whiteLogo
         hasBelt
         className="items-start text-left"
-        title="A real time<br>tower defence PVP game"
+        title="A real time<br>tower defence strategy game"
         subtitle="AstrArk is an exciting mobile tower defense game that's all about fun and strategy. Dive into thrilling PvP and PvE<br>battles with your friends.<br>In AstrArk, you get to choose your own commander, assemble your dream team, and build your squad with tactical<br>formations. Whether you're up for a quick brawl or a challenging showdown, there are various battle modes to keep you<br>entertained."
       />
     </div>
