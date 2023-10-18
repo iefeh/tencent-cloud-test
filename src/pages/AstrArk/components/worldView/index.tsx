1d77bcbd0fa4d4bd3291df54be4c93e8787a0084
duanleiyu <ichinoseeki@outlook.com>
2023-10-19 00:55:42 +0800

    feat: 优化world view文字布局

--- src/pages/AstrArk/components/worldView/index.tsx
+++ src/pages/AstrArk/components/worldView/index.tsx
@@ -2,6 +2,48 @@ import React from 'react';
 import PageDesc from '../../../components/common/PageDesc';
 
 const WorldView: React.FC = () => {
+  const Content = () => (
+    <>
+      <video
+        className="object-cover absolute left-0 top-0 w-full h-full z-0"
+        autoPlay
+        playsInline
+        muted
+        loop
+        preload="auto"
+      >
+        <source src="/video/meteorite.mp4" />
+      </video>
+
+      <div className="bg-black/30 absolute inset-0 z-0"></div>
+
+      <PageDesc
+        hasBelt
+        title="What happened..."
+        subtitle={
+          <div className="title text-lg font-decima mb-10 tracking-tighter">
+            <span className='max-md:inline-block hidden'>
+              A mysterious object offering infinite energy has reshaped our world. As humanity studies its enigmatic
+              nature, humanity finds itself enslaved by the object while averting energy shortages. But when it
+              vanished, chaos ensued. Nations crumbled, and it was called the &quot;End Times.&quot;
+            </span>
+
+            <span className="max-md:hidden">
+              A mysterious object offering infinite energy has reshaped
+              <br /> our world. As humanity studies its enigmatic nature,
+              <br /> humanity finds itself enslaved by the object while
+              <br /> averting energy shortages.
+              <br />
+              <br />
+              But when it vanished, chaos ensued. Nations crumbled, and
+              <br /> it was called the &quot;End Times.&quot;
+            </span>
+          </div>
+        }
+      />
+    </>
+  );
+
   return (
     <div className="worldView w-full h-screen relative flex justify-center items-center overflow-hidden">
       <div
@@ -9,25 +51,7 @@ const WorldView: React.FC = () => {
         data-speed-y="8"
         data-offset="-300"
       >
-        <video
-          className="object-cover absolute left-0 top-0 w-full h-full z-0"
-          autoPlay
-          playsInline
-          muted
-          loop
-          preload="auto"
-        >
-          <source src="/video/meteorite.mp4" />
-        </video>
-
-        <div className="bg-black/30 absolute inset-0 z-0"></div>
-
-        <PageDesc
-          hasBelt
-          title={<div className='title text-[6.25rem] uppercase font-semakin mb-4'>What happened...</div>}
-          subtitle='A mysterious object offering infinite energy has reshaped<br/> our world. As humanity studies its enigmatic nature,<br/> humanity finds itself enslaved by the object while<br/> averting energy shortages.
-        <br/><br/>But when it vanished, chaos ensued. Nations crumbled, and<br/> it was called the "End Times."'
-        />
+        <Content />
       </div>
 
       <div
@@ -35,24 +59,7 @@ const WorldView: React.FC = () => {
         data-speed-y="2"
         data-offset="-100"
       >
-        <video
-          className="object-cover absolute left-0 top-0 w-full h-full z-0"
-          autoPlay
-          playsInline
-          muted
-          loop
-          preload="auto"
-        >
-          <source src="/video/meteorite.mp4" />
-        </video>
-
-        <div className="bg-black/30 absolute inset-0 z-0"></div>
-
-        <PageDesc
-          hasBelt
-          title="What happened..."
-          subtitle={<div className='title text-lg font-decima mb-10 tracking-tighter text-justify'>A mysterious object offering infinite energy has reshaped our world. As humanity studies its enigmatic nature, humanity finds itself enslaved by the object while averting energy shortages. But when it vanished, chaos ensued. Nations crumbled, and it was called the &quot;End Times.&quot;</div>}
-        />
+        <Content />
       </div>
     </div>
   );
