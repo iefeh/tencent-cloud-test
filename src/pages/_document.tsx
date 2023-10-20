41cd2c1f7ee16dd2b5f3a70d5fcfbeb2e6e6f51a
Eki <ichinoseeki@outlook.com>
2023-10-20 12:27:52 +0800

    feat: 添加网站图标

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head title="Moonveil">
         <meta name="description" content="Moonveil Official Website" />
+        <link rel="icon" href="img/favicon.png" type="image/x-icon"></link>
+		    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon"></link>
         <script src="https://accounts.google.com/gsi/client" async defer></script>
         <script type="text/javascript" async src="https://platform.twitter.com/widgets.js"></script>
       </Head>
