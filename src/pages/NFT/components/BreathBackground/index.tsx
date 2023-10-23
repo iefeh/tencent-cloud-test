2b4dd0dd408e119a8ec9992c8e002d77d3bf6c52
Eki <ichinoseeki@outlook.com>
2023-10-24 00:48:27 +0800

    feat: 实现简易呼吸动画

--- src/pages/NFT/components/BreathBackground/index.tsx
+++ src/pages/NFT/components/BreathBackground/index.tsx
@@ -1,8 +1,13 @@
-import Image from "next/image";
+import Image from 'next/image';
 import triangleImg from 'img/nft/breathBg/triangle.png';
+import breathImg from 'img/nft/breathBg/breath.png';
+import styles from './index.module.css';
 
 export default function BreathBackground() {
-  return <div className="fixed w-screen h-screen left-0 top-0 z-0 flex justify-center items-center">
-    <Image className="w-[44.875rem] h-[41.5rem]" src={triangleImg} alt="" />
-  </div>;
+  return (
+    <div className="fixed w-screen h-screen left-0 top-0 z-0 flex justify-center items-center">
+      <Image className="w-[44.875rem] h-[41.5rem]" src={triangleImg} alt="" />
+      <Image className={"w-[120rem] absolute left-0 top-[61.5%] origin-center " + styles.breath} src={breathImg} alt="" />
+    </div>
+  );
 }
