dc767a812e11d8d58362af7676d72838148dbb45
段雷雨 <duanleiyu@trinasky.com>
2023-10-27 16:03:36 +0800

    feat: 优化auth后token取值逻辑

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,12 +1,10 @@
 import { KEY_AUTHORIZATION } from "@/constant/storage";
-import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function Auth() {
-  const router = useRouter();
-
   useEffect(() => {
-    const token = router.query.token as string;
+    const query = new URLSearchParams(location.search);
+    const token = query.get('token') || '';
     localStorage.setItem(KEY_AUTHORIZATION, token);
   }, []);
 
