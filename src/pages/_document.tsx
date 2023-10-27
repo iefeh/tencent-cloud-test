b9e2eb80450f8b45e07f5b475d8677052e2005f3
段雷雨 <duanleiyu@trinasky.com>
2023-10-27 11:36:50 +0800

    feat: 修改seo信息

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head title="Moonveil">
-        <meta name="description" content="Moonveil Official Website" />
+      <Head title="Moonveil Entertainment">
+        <meta name="description" content="Empowered by cutting-edge technologies , our mission is to craft top-notch gaming experiences that seamlessly combine casual flexibility with authentic fun depth." />
         <link rel="icon" href="img/favicon.png" type="image/x-icon"></link>
 		    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon"></link>
         <script src="https://accounts.google.com/gsi/client" async defer></script>
