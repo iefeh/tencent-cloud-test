a96dd5e82eac882a8173b58b9b99c18588f29c6d
duanleiyu <ichinoseeki@outlook.com>
2023-12-18 11:08:44 +0800

    feat: add my nft content.

---
+++ src/pages/Profile/components/MyNFT.tsx
@@ -0,0 +1,37 @@
+import { Divider } from '@nextui-org/react';
+import NFT from '@/pages/components/common/nft/NFT';
+import baseNFTImg from 'img/nft/common/nft_base.jpg';
+import { useState } from 'react';
+
+export default function MyNFT() {
+  const [nfts, setNFTs] = useState([
+    {
+      title: 'Destiny Tetra : 001',
+      cover: baseNFTImg,
+    },
+    {
+      title: 'Destiny Tetra : 001',
+      cover: baseNFTImg,
+    },
+    {
+      title: 'Destiny Tetra : 001',
+      cover: baseNFTImg,
+    },
+    null,
+    null
+  ]);
+
+  return (
+    <div className="">
+      <div className="mt-20 font-semakin text-2xl text-basic-yellow">My NFT</div>
+
+      <Divider className="my-[1.875rem]" />
+
+      <div className="flex justify-between items-center">
+        {nfts.map((nft, index) => (
+          <NFT src={nft?.cover} key={index} />
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -4,12 +4,13 @@ import MoonBeams from './components/MoonBeams';
 import MyBadges from './components/MyBadges';
 import DailyCheckIn from '../LoyaltyProgram/earn/components/EarnBanner/DailyCheckIn';
 import Invite from '../LoyaltyProgram/earn/components/EarnBanner/Invite';
+import MyNFT from './components/MyNFT';
 
 export default function ProfilePage() {
   return (
     <section
       id="luxy"
-      className="w-full flex flex-col px-[16.25rem] pt-[12.5rem] mx-auto min-h-screen bg-[url('/img/profile/bg.png')] bg-[length:100%_auto] bg-no-repeat"
+      className="w-full flex flex-col px-[16.25rem] pt-[12.5rem] pb-[16rem] mx-auto min-h-screen bg-[url('/img/profile/bg.png')] bg-[length:100%_auto] bg-no-repeat"
     >
       <Head>
         <title>User Center | Moonveil Entertainment</title>
@@ -26,6 +27,8 @@ export default function ProfilePage() {
 
         <Invite />
       </div>
+      
+      <MyNFT />
     </section>
   );
 }
