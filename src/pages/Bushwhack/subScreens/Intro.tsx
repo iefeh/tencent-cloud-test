1966b57b0360cd8c1330769fc6a9e4de099c52ba
Eki <ichinoseeki@outlook.com>
2023-12-26 15:54:50 +0800

    fix: model property error.

--- src/pages/Bushwhack/subScreens/Intro.tsx
+++ src/pages/Bushwhack/subScreens/Intro.tsx
@@ -8,16 +8,12 @@ import { Button, useDisclosure } from '@nextui-org/react';
 interface Role {
   name: string;
   cover: StaticImageData;
-  source: string;
-  texture: string;
-  mask: {
-    source: string;
-    texture: string;
-  };
+  model: ModelInfo;
+  mask: ModelInfo;
 }
 
 function RoleCell(props: Role) {
-  const { cover, source, texture, mask } = props;
+  const { cover, model, mask } = props;
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const { isOpen: isOpenMask, onOpen: onOpenMask, onOpenChange: onOpenChangeMask } = useDisclosure();
 
@@ -26,25 +22,22 @@ function RoleCell(props: Role) {
       <Image src={cover} alt="" onClick={onOpen} />
       <Button onPress={onOpenMask}>Mask</Button>
 
-      <ModelView3DModal isOpen={isOpen} onOpenChange={onOpenChange} source={source} texture={texture} />
+      <ModelView3DModal isOpen={isOpen} onOpenChange={onOpenChange} info={model} />
 
-      <ModelView3DModal
-        isOpen={isOpenMask}
-        onOpenChange={onOpenChangeMask}
-        source={mask.source}
-        texture={mask.texture}
-      />
+      <ModelView3DModal isOpen={isOpenMask} onOpenChange={onOpenChangeMask} info={model} />
     </div>
   );
 }
 
 export default function IntroScreen() {
-  const roles = [
+  const roles: Role[] = [
     {
       name: 'Doctor',
       cover: doctorImg,
-      source: '/models/Lewis_H.fbx',
-      texture: '/models/textures/Lewis.tga',
+      model: {
+        source: '/models/Lewis_H.fbx',
+        texture: '/models/textures/Lewis.tga',
+      },
       mask: {
         source: '/models/lewis_mask.fbx',
         texture: '/models/textures/Lewis.tga',
@@ -53,8 +46,10 @@ export default function IntroScreen() {
     {
       name: 'Rhea',
       cover: rheaImg,
-      source: '/models/Rhea_H.fbx',
-      texture: '/models/textures/T_Rhea.tga',
+      model: {
+        source: '/models/Rhea_H.fbx',
+        texture: '/models/textures/T_Rhea.tga',
+      },
       mask: {
         source: '/models/rhea_mask.fbx',
         texture: '/models/textures/T_Rhea.tga',
@@ -63,8 +58,10 @@ export default function IntroScreen() {
     {
       name: 'Thief',
       cover: thiefImg,
-      source: '/models/Thief_H.fbx',
-      texture: '/models/textures/chaowan.tga',
+      model: {
+        source: '/models/Thief_H.fbx',
+        texture: '/models/textures/chaowan.tga',
+      },
       mask: {
         source: '/models/chaowan_mianju.fbx',
         texture: '/models/textures/chaowan.tga',
