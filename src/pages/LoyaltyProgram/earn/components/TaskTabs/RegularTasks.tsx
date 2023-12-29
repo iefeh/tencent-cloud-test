a775eecc5ee592b4ee064213458f03bd12c2c451
Eki <ichinoseeki@outlook.com>
2023-12-29 10:47:58 +0800

    feat: add view more.

--- src/pages/LoyaltyProgram/earn/components/TaskTabs/RegularTasks.tsx
+++ src/pages/LoyaltyProgram/earn/components/TaskTabs/RegularTasks.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import mbImg from 'img/loyalty/earn/mb.png';
-import { CircularProgress, Pagination } from '@nextui-org/react';
+import { CircularProgress, Pagination, cn } from '@nextui-org/react';
 import PaginationRenderItem from './components/PaginationRenderItem';
 import ConnectAndVerify, { VerifyTexts } from '@/pages/components/common/buttons/ConnectAndVerify';
 import { TaskListItem, TaskReward, queryTaskListAPI } from '@/http/services/task';
@@ -8,6 +8,7 @@ import { useEffect, useState } from 'react';
 import { QuestRewardType, QuestType } from '@/constant/task';
 import { connectDiscordAPI, connectSteamAPI, connectTwitterAPI } from '@/http/services/login';
 import { useWeb3Modal, useWeb3ModalAccount } from '@web3modal/ethers/react';
+import closeImg from 'img/loyalty/earn/close.png';
 
 interface TaskItem extends TaskListItem {
   connectTexts?: VerifyTexts;
@@ -117,48 +118,88 @@ export default function RegularTasks() {
     queryTasks();
   }, []);
 
+  const Task = (props: { task: TaskItem }) => {
+    const { task } = props;
+    const [isContentVisible, setIsContentVisibble] = useState(false);
+
+    function showContent() {
+      setIsContentVisibble(true);
+    }
+
+    function hideContent() {
+      setIsContentVisibble(false);
+    }
+
+    return (
+      <div className="task-item col-span-1 overflow-hidden border-1 border-basic-gray rounded-[0.625rem] min-h-[17.5rem] pt-[2.375rem] px-[2.375rem] pb-[2.5rem] flex flex-col hover:border-basic-yellow transition-[border-color] duration-500">
+        <div className="text-xl">{task.name}</div>
+
+        <div className="mt-3 flex-1 flex flex-col justify-between relative">
+          <div className="text-sm">
+            <div className="text-[#999]">{task.description}</div>
+            {task.tip && (
+              <div className="flex items-center">
+                <div className="flex-1 text-[#999] overflow-hidden whitespace-nowrap text-ellipsis">{task.tip}</div>
+                <div className="text-basic-yellow shrink-0 cursor-pointer leading-6 border-b-1 border-basic-yellow" onClick={showContent}>
+                  View More
+                </div>
+              </div>
+            )}
+          </div>
+
+          <div className="footer">
+            <div className="flex items-center">
+              <Image className="w-8 h-8" src={mbImg} alt="" />
+
+              <span className="font-semakin text-base text-basic-yellow ml-[0.4375rem]">
+                {getRewardText(task.reward)}
+              </span>
+            </div>
+
+            <div className="mt-5">
+              <ConnectAndVerify
+                type={task.type}
+                // init={() => onInit(task)}
+                connectTexts={task.connectTexts}
+                verifyTexts={task.verifyTexts}
+                connect={task.onConnectClick}
+                verify={task.onVerifyClick}
+              />
+            </div>
+          </div>
+
+          <div
+            className={cn([
+              'absolute z-0 w-full h-full left-0 top-0 overflow-hidden bg-black transition-[max-height] ease-in-out !duration-500',
+              isContentVisible ? 'max-h-full' : 'max-h-0 pointer-events-none',
+            ])}
+          >
+            <div className="w-full h-full rounded-[0.625rem] pt-8 px-6 pb-4 bg-[#141414]">
+              <div className="text-sm text-white">{task.description}</div>
+              <div className="text-sm text-[#999] mt-[0.625rem]">{task.tip}</div>
+            </div>
+
+            <Image
+              className="w-3 h-3 absolute right-[0.625rem] top-[0.625rem] cursor-pointer"
+              src={closeImg}
+              alt=""
+              onClick={hideContent}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="mt-7 flex flex-col items-center">
       {tasks.length < 1 ? (
         <CircularProgress aria-label="Loading..." />
       ) : (
         <div className="content grid grid-cols-3 gap-[1.5625rem] font-poppins w-full">
-          {tasks.map((task) => {
-            return (
-              <div
-                key={`${task.id}_${task.achieved}`}
-                className="task-item col-span-1 overflow-hidden border-1 border-basic-gray rounded-[0.625rem] min-h-[17.5rem] pt-[2.375rem] px-[2.375rem] pb-[2.5rem] flex flex-col justify-between hover:border-basic-yellow transition-[border-color] duration-500"
-              >
-                <div>
-                  <div className="text-xl">{task.name}</div>
-
-                  <div className="text-sm text-[#999]">{task.description}</div>
-                  <div className="text-sm text-[#999] overflow-hidden whitespace-nowrap text-ellipsis">{task.tip}</div>
-                </div>
-
-                <div className="footer">
-                  <div className="flex items-center">
-                    <Image className="w-8 h-8" src={mbImg} alt="" />
-
-                    <span className="font-semakin text-base text-basic-yellow ml-[0.4375rem]">
-                      {getRewardText(task.reward)}
-                    </span>
-                  </div>
-
-                  <div className="mt-5">
-                    <ConnectAndVerify
-                      type={task.type}
-                      // init={() => onInit(task)}
-                      connectTexts={task.connectTexts}
-                      verifyTexts={task.verifyTexts}
-                      connect={task.onConnectClick}
-                      verify={task.onVerifyClick}
-                    />
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+          {tasks.map((task) => (
+            <Task key={`${task.id}_${task.achieved}`} task={task} />
+          ))}
         </div>
       )}
 
