1b6d3d99c8b45b1cfc3e1bdaa193799671f64cde
Eki <ichinoseeki@outlook.com>
2024-01-11 10:24:51 +0800

    fix: suspense error.

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,15 +1,15 @@
 import { KEY_AUTHORIZATION, KEY_PARTICLE_TOKEN } from '@/constant/storage';
-import { useEffect } from 'react';
+import { startTransition } from 'react';
 
 export default function Auth() {
-  useEffect(() => {
+  startTransition(() => {
     const query = new URLSearchParams(location.search);
     const token = query.get('token') || '';
     const jwt = query.get('particle_jwt') || '';
     localStorage.setItem(KEY_AUTHORIZATION, token);
     localStorage.setItem(KEY_PARTICLE_TOKEN, jwt);
     window.close();
-  }, []);
+  });
 
   return null;
 }
