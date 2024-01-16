f951133f8e86ef43982a12a6a931cb1e38c1ad30
Eki <ichinoseeki@outlook.com>
2024-01-16 13:06:11 +0800

    feat: download responsive.

--- src/pages/AstrArk/Download/index.tsx
+++ src/pages/AstrArk/Download/index.tsx
@@ -32,7 +32,7 @@ export default function DownloadPage() {
         <Image className="object-cover z-10 opacity-100" src={maskBg} alt="" fill />
       </div>
 
-      <div className=" absolute left-1/2 -translate-x-1/2 bottom-[12.6%] flex items-center">
+      <div className=" absolute left-1/2 -translate-x-1/2 bottom-[12.6%] flex flex-col md:flex-row items-center gap-[0.625rem]">
         <Button
           className="w-[19.6875rem] h-[4.375rem] bg-[url('/img/astrark/pre-register/bg_btn_colored.png')] bg-cover bg-no-repeat !bg-transparent font-semakin text-black text-2xl"
           disableRipple
@@ -42,7 +42,7 @@ export default function DownloadPage() {
         </Button>
 
         <Button
-          className="w-[13.25rem] h-[4.375rem] bg-[url('/img/astrark/btn_google_play.png')] bg-cover bg-no-repeat !bg-transparent ml-[0.625rem]"
+          className="w-[13.25rem] h-[4.375rem] bg-[url('/img/astrark/btn_google_play.png')] bg-cover bg-no-repeat !bg-transparent"
           disableRipple
           onPress={onGooglePlayClick}
         />
--- src/pages/AstrArk/components/FloatRegisterButton.tsx
+++ src/pages/AstrArk/components/FloatRegisterButton.tsx
@@ -13,7 +13,7 @@ export default function FloatRegisterButton() {
 
   return createPortal(
     <Button
-      className="w-[17.75rem] h-[10.25rem] bg-[url('/img/astrark/bg_register_now.png')] bg-cover bg-no-repeat bg-transparent fixed left-[4.875rem] bottom-[4.875rem] z-20 font-semakin text-[1.75rem] text-left hidden md:block"
+      className="w-[17.75rem] h-[10.25rem] bg-[url('/img/astrark/bg_register_now.png')] bg-cover bg-no-repeat bg-transparent fixed lg:left-[4.875rem] lg:bottom-[4.875rem] z-20 font-semakin text-[1.75rem] text-left left-6 bottom-8 scale-[0.6] origin-bottom-left lg:scale-100"
       disableRipple
       onPress={onFloatClick}
     >
