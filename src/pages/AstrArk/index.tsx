ad4ea6f1aaee693403210cf2d9efa5dd82df26a1
Eki <ichinoseeki@outlook.com>
2024-01-17 19:06:21 +0800

    feat: hide register entry.

--- src/pages/AstrArk/index.tsx
+++ src/pages/AstrArk/index.tsx
@@ -42,7 +42,7 @@ export default function Home() {
           <AstrArkSchoolDesc />
         </div>
 
-        <FloatRegisterButton />
+        {/* <FloatRegisterButton /> */}
       </section>
 
       <AstrarkHome scrollY={scrollY} />
