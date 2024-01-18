c9ef00831be03d6a17276f9a0385ad362862fa2f
Eki <ichinoseeki@outlook.com>
2024-01-18 17:08:42 +0800

    style: connect tips modal.

--- src/hooks/useConnect.tsx
+++ src/hooks/useConnect.tsx
@@ -32,7 +32,7 @@ export default function useConnect(type: string, callback?: (args?: any) => void
         toast.error(msg);
       }
 
-      if ([-9, -13, -16].includes(+code)) {
+      if ([-1, -9, -13, -16].includes(+code)) {
         onOpen();
       }
     }
@@ -126,26 +126,38 @@ export default function useConnect(type: string, callback?: (args?: any) => void
         backdrop="blur"
         isOpen={isOpen}
         onOpenChange={onOpenChange}
-        classNames={{ base: 'bg-[#141414] rounded-base max-w-40rem' }}
+        classNames={{ base: 'bg-[#141414] rounded-base max-w-[40rem]' }}
       >
         <ModalContent>
           {(onClose) => (
             <ModalBody>
-              <div className="flex flex-col items-center pt-12 pb-10">
+              <div className="flex flex-col" style={{ paddingTop: '3rem', paddingBottom: '2.5rem' }}>
                 <p>
                   We have detected an account connection conflict. This occurs when an attempt is made to link a
-                  platform that is already connected to another account. To resolve this, please follow these steps:
+                  platform that is already connected to another account.
                 </p>
-                <p>1. Log in with the platform that is currently linked to the conflicting account.</p>
-                <p>2. Go to [User Center], and disconnect the platform account causing the conflict.</p>
-                <p>
+                <br />
+                <p>To resolve this, please follow these steps:</p>
+                <br />
+                <p style={{ textIndent: '1em' }}>
+                  1. Log in with the platform that is currently linked to the conflicting account.
+                </p>
+                <p style={{ textIndent: '1em' }}>
+                  2. Go to [User Center], and disconnect the platform account causing the conflict.
+                </p>
+                <p style={{ textIndent: '1em' }}>
                   3. Wait for 12 hours before attempting to reconnect. This cooldown period helps us maintain platform
                   integrity and prevent abusive practices.
                 </p>
-                <p>4. After 12 hours, log in again and link the account you wish to use.</p>
+                <p style={{ textIndent: '1em' }}>
+                  4. After 12 hours, log in again and link the account you wish to use.
+                </p>
+                <br />
                 <p>If you encounter any issues, please contact our support team for assistance.</p>
 
-                <LGButton className="mt-4" actived label="Close" onClick={onClose} />
+                <div className="mt-4 flex justify-center">
+                  <LGButton actived label="Close" onClick={onClose} />
+                </div>
               </div>
             </ModalBody>
           )}
