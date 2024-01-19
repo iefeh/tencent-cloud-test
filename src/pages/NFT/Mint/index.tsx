f057133d46d976adfd06d145b4b3e35d2df90e50
Eki <ichinoseeki@outlook.com>
2024-01-19 11:56:52 +0800

    feat: add mint page & button.

---
+++ src/pages/NFT/Mint/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function MintPage() {
+  return (
+    <section id="luxy">
+      <Head>
+        <title>NFT | Moonveil Entertainment</title>
+      </Head>
+
+      <div className="w-screen h-screen bg-[url('/img/nft/mint/bg.jpg')] bg-cover bg-no-repeat"></div>
+    </section>
+  );
+}
