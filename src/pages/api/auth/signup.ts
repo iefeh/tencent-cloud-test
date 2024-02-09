b788b0c7dc5392f02863ec5ddce11b514f5f8865
heyuanyou <harrell16805@163.com>
2024-02-09 13:54:24 +0800

    check signup conflict

--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -15,10 +15,14 @@ import UserTwitter from "@/lib/models/UserTwitter";
 import UserWallet from "@/lib/models/UserWallet";
 import {generateUserSession} from "@/lib/middleware/session";
 import {genLoginJWT} from "@/lib/particle.network/auth";
+import {isDuplicateKeyError} from "@/lib/mongodb/client";
+import {errorInterceptor} from "@/lib/middleware/error";
+import {timeoutInterceptor} from "@/lib/middleware/timeout";
+import logger from "@/lib/logger/winstonLogger";
 
 const router = createRouter<UserContextRequest, NextApiResponse>();
 
-router.post(async (req, res) => {
+router.use(errorInterceptor(), timeoutInterceptor()).post(async (req, res) => {
     const {signup_cred} = req.body;
     if (!signup_cred) {
         return res.json(response.invalidParams());
@@ -30,38 +34,47 @@ router.post(async (req, res) => {
     }
     const payload = JSON.parse(payloadStr) as SignupPayload;
     // 保存用户信息
-    await doTransaction(async function (session) {
-        const opts = {session};
-        const user = new User(payload.user);
-        await user.save(opts);
-        if (payload.third_party_user) {
-            let userConnection;
-            switch (payload.authorization_type) {
-                case AuthorizationType.Discord:
-                    userConnection = new UserDiscord(payload.third_party_user);
-                    break;
-                case AuthorizationType.Google:
-                    userConnection = new UserGoogle(payload.third_party_user);
-                    break;
-                case AuthorizationType.Steam:
-                    userConnection = new UserSteam(payload.third_party_user);
-                    break;
-                case AuthorizationType.Twitter:
-                    userConnection = new UserTwitter(payload.third_party_user);
-                    break;
-                case AuthorizationType.Wallet:
-                    userConnection = new UserWallet(payload.third_party_user);
-                    break;
-                default:
-                    throw new Error("unimplemented authorization type.");
+    try {
+        await doTransaction(async function (session) {
+            const opts = {session};
+            const user = new User(payload.user);
+            await user.save(opts);
+            if (payload.third_party_user) {
+                let userConnection;
+                switch (payload.authorization_type) {
+                    case AuthorizationType.Discord:
+                        userConnection = new UserDiscord(payload.third_party_user);
+                        break;
+                    case AuthorizationType.Google:
+                        userConnection = new UserGoogle(payload.third_party_user);
+                        break;
+                    case AuthorizationType.Steam:
+                        userConnection = new UserSteam(payload.third_party_user);
+                        break;
+                    case AuthorizationType.Twitter:
+                        userConnection = new UserTwitter(payload.third_party_user);
+                        break;
+                    case AuthorizationType.Wallet:
+                        userConnection = new UserWallet(payload.third_party_user);
+                        break;
+                    default:
+                        throw new Error("unimplemented authorization type.");
+                }
+                await userConnection.save(opts);
             }
-            await userConnection.save(opts);
+            if (payload.invite) {
+                const invite = new UserInvite(payload.invite);
+                await invite.save(opts);
+            }
+        });
+    } catch (e) {
+        if (isDuplicateKeyError(e)) {
+            logger.warn(e);
+            res.json(response.signupConfirmationExpired());
+            return;
         }
-        if (payload.invite) {
-            const invite = new UserInvite(payload.invite);
-            await invite.save(opts);
-        }
-    })
+        throw e;
+    }
     // 删除注册凭证
     await redis.del(`user_signup_confirmation:${signup_cred}`);
     // 生成登录token
