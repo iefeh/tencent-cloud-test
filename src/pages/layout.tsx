c367fcf63b27d95d20a2a2702703d00750e2b64c
Eki <ichinoseeki@outlook.com>
2024-02-20 09:45:47 +0800

    feat: no loading on event details page.

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,20 +1,14 @@
-import React from "react";
-import Header from "./components/common/Header";
-import LineBorder from "./components/home/LineBorder/index";
-import { Suspense } from "react";
-import Loading from "./components/common/Loading";
-import "swiper/css";
+import React from 'react';
+import Header from './components/common/Header';
+import LineBorder from './components/home/LineBorder/index';
+import { Suspense } from 'react';
+import Loading from './components/common/Loading';
+import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/autoplay';
 import 'swiper/css/navigation';
 
-export default function RootLayout({
-  children,
-  isInWhiteList
-}: {
-  children: React.ReactNode;
-  isInWhiteList: boolean;
-}) {
+export default function RootLayout({ children, isInWhiteList }: { children: React.ReactNode; isInWhiteList: boolean }) {
   return (
     <React.Fragment>
       <LineBorder />
@@ -23,11 +17,9 @@ export default function RootLayout({
         className="flex w-full h-screen flex-col items-center justify-between relative bg-black"
         id="main-layout"
       > */}
-        <Suspense fallback={<Loading />}>
-          {isInWhiteList || <Header />}
+      {isInWhiteList || <Header />}
 
-          <main className="dark page-container w-full h-full">{children}</main>
-        </Suspense>
+      <main className="dark page-container w-full h-full">{children}</main>
       {/* </main> */}
     </React.Fragment>
   );
