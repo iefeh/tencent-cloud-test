935cdf39663dd81a34e33523abd775293d9cfbdb
iefeh <inoti@qq.com>
2024-03-18 21:46:21 +0800

    Optimize list api's query logic and add getClaimedBadges api.

--- src/pages/api/badges/list.ts
+++ src/pages/api/badges/list.ts
@@ -4,7 +4,8 @@ import * as response from '@/lib/response/response';
 import { maybeAuthInterceptor, mustAuthInterceptor, UserContextRequest } from '@/lib/middleware/auth';
 import UserBadges, { IUserBadges } from '@/lib/models/UserBadges';
 import Badges, { IBadges } from '@/lib/models/Badge';
-import { PipelineStage } from 'mongoose';
+import { PipelineStage, StringExpressionOperator } from 'mongoose';
+import { Data3DTexture } from 'three';
 
 const router = createRouter<UserContextRequest, NextApiResponse>();
 
@@ -32,160 +33,56 @@ router.use(mustAuthInterceptor).get(async (req, res) => {
     return;
   }
 
-  let [badges, claimed_count] = await loadUserBadges(userId, pageNum, pageSize);
-  let ownedIds: any[] = [];
-  if (badges[0].data.length < pageSize) {
-    if (badges[0].data.length == 0) {
-      const result = await loadUserBadgesCount(userId);
-      let skip: number;
-      if (result[0].data.length == 0) {
-        skip = 0;
-      } else {
-        skip = (pageNum - 1) * pageSize - result[0].data.length;
-        for (let c of result[0].data) {
-          ownedIds.push(c.badge_id);
-        }
-      }
-      badges = await loadAllBadge(skip, pageSize, ownedIds);
-    } else {
-      for (let c of badges[0].data) {
-        ownedIds.push(c.badge_id);
-      }
-      const temp = await loadAllBadge(0, pageSize - badges[0].data.length, ownedIds);
-
-      for (let c of temp[0].data) {
-        badges[0].data.push(c);
-      }
-    }
-  }
+  let [badges, claimed_count] = await loadAllBadges(userId, pageNum, pageSize);
   //返回用户徽章
   res.json(
     response.success({
-      total: badges[0].data.length,
-      claimed_count,
-      badges: badges[0].data,
+      total: badges.length,
+      claimed_count: claimed_count,
+      badges: badges,
     }),
   );
 });
 
-export async function loadUserBadges(userId: string, pageNum: number, pageSize: number): Promise<any[]> {
+export async function loadAllBadges(userId: string, pageNum: number, pageSize: number): Promise<[any[], number]> {
   const skip = (pageNum - 1) * pageSize;
-  let claimed_count = 0; //已领取徽章计数
-  let claimed: boolean;
-  const aggregateQuery: PipelineStage[] = [
-    {
-      $match: {
-        user_id: userId,
-      },
-    },
-    {
-      $sort: {
-        // 按照'order'升序排序
-        order: 1,
-      },
-    },
-    {
-      $project: {
-        _id: 0,
-      },
-    },
-    {
-      $facet: {
-        data: [{ $skip: skip }, { $limit: pageSize }],
-      },
-    },
-  ];
-
-  const badges = await UserBadges.aggregate(aggregateQuery);
-
-  for (let c of badges[0].data) {
-    //查询徽章配置
-    let b = await Badges.find({ id: c.badge_id });
-    if (b[0] == undefined) {
-      continue;
-    }
-
-    //获取用户最高等级徽章
-    let lv = -Infinity; //不能使用Number.MIN_VALUE指定为最小值，当LV是0时会异常
-    //分别获取领取的最高等级和未领取的最高等级
-    claimed = false;
-    for (let k of Object.keys(c.series)) {
-      if (c.series[k].claimed_time != null) {
-        claimed = true;
-      }
-      if (Number(k) > lv) {
-        lv = Number(k);
-      }
-    }
-    if (claimed) {
-      claimed_count++;
-    }
-
-    //添加徽章的信息
-    let displayedBadges: any = [];
-    let t = b[0].series.get(String(lv));
-
-    displayedBadges.push({
-      lv: lv,
-      name: b[0].name,
-      description: t.description,
-      icon_url: t.icon_url,
-      image_url: t.image_url,
-      obtained_time: c.series[String(lv)].obtained_time,
-      claimed_time: c.series[String(lv)].claimed_time,
-    });
-
-    t = b[0].series.get(String(lv + 1));
-    if (t != undefined) {
-      displayedBadges.push({
-        lv: lv + 1,
-        name: b[0].name,
-        description: t.description,
-        icon_url: t.icon_url,
-        image_url: t.image_url,
-        // obtained_time: undefined,
-        // claimed_time: undefined,
-      });
-    } else {
-      displayedBadges.push(null);
-    }
-    //delete c.series;
-    c.series = displayedBadges;
-
-    //是否为系列徽章
-    if (Array.from(b[0].series.keys()).length > 1) {
-      c.has_series = true;
-    } else {
-      c.has_series = false;
-    }
-  }
-  return [badges, claimed_count];
-}
-
-export async function loadAllBadge(skip: number, pageSize: number, ownedIds: any[]): Promise<any[]> {
   const aggregateQuery: PipelineStage[] = [
     {
       $match: {
         active: true,
-        id: { $nin: ownedIds },
+      },
+    },
+    {
+      $lookup: {
+        from: 'user_badges',
+        let: { badge_id: '$id' },
+        pipeline: [
+          // 联表时过滤已删除的记录
+          {
+            $match: { $expr: { $and: [{ $eq: ['$user_id', userId] }, { $eq: ['$badge_id', '$$badge_id'] }] } },
+          },
+        ],
+        as: 'user_badges',
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        id: 1,
+        name: 1,
+        series: 1,
+        order: 1,
+        created_time: 1,
+        updated_time: 1,
+        user_badges: '$user_badges',
+        item_count: { $size: '$user_badges' },
       },
     },
     {
       $sort: {
         // 按照'order'升序排序
+        item_count: -1,
         order: 1,
-      },
-    },
-    {
-      $project: {
-        //id:"$badge_id",
-        _id: 0,
-        __v: 0,
-        chain_id: 0,
-        deleted_time: 0,
-        description: 0,
-        obtain_url: 0,
-        active: 0,
       },
     },
     {
@@ -194,55 +91,75 @@ export async function loadAllBadge(skip: number, pageSize: number, ownedIds: any
       },
     },
   ];
+  const results = await Badges.aggregate(aggregateQuery);
+  let data: any[] = [];
+  let claimed_count: number = 0;
+  let claimed: boolean;
+  let maxLv: number;
+  if (results.length > 0) {
+    for (let c of results[0].data) {
+      let series: any = [];
+      if (c.user_badges.length > 0) {
+        c.has_series = Object.keys(c.series).length > 1;
 
-  const badges = await Badges.aggregate(aggregateQuery);
-  let lv: number;
-  for (let c of badges[0].data) {
-    c['badge_id'] = c.id;
-    //获取最低等级
-    lv = Number.MAX_VALUE;
-    for (let k of Object.keys(c.series)) {
-      if (Number(k) < lv) {
-        lv = Number(k);
+        maxLv = -Infinity;
+        //获取用户最大等级
+        claimed = false;
+        for (let k of Object.keys(c.user_badges[0].series)) {
+          if (Number(k) > maxLv) {
+            maxLv = Number(k);
+          }
+          if (c.user_badges[0].series[k].claimed_time != null) {
+            claimed = true;
+          }
+        }
+        if (claimed) {
+          claimed_count++;
+        }
+
+        let distributedBadge = c.user_badges[0].series[String(maxLv)];
+        let orgBadgeData = c.series[String(maxLv)];
+        let nextLvBadge = c.series[String(maxLv + 1)];
+        distributedBadge.lv = maxLv;
+        distributedBadge.name = c.name;
+        distributedBadge.description = orgBadgeData.description;
+        distributedBadge.icon_url = orgBadgeData.icon_url;
+        distributedBadge.image_url = orgBadgeData.image_url;
+        series.push(distributedBadge);
+        c.display = c.user_badges[0].display;
+        c.display_order = c.user_badges[0].display_order;
+        if (nextLvBadge != undefined) {
+          delete nextLvBadge.requirements;
+          delete nextLvBadge.open_for_mint;
+          nextLvBadge.name = c.name;
+          nextLvBadge.lv = maxLv + 1;
+          series.push(nextLvBadge);
+        } else {
+          series.push(null);
+        }
+        c.user_id = distributedBadge.user_id;
+      } else {
+        c.has_series = Object.keys(c.series).length > 1;
+        let k = Object.keys(c.series)[0];
+        c.series[k].lv = Number(k);
+        c.series[k].name = c.name;
+        delete c.series[k].requirements;
+        delete c.series[k].open_for_mint;
+
+        series.push(c.series[k]);
+        series.push(null);
       }
+      c.badge_id = c.id;
+      delete c.user_badges;
+      delete c.id;
+      delete c.item_count;
+      c.series = series;
+      data.push(c);
     }
-
-    let displayedBadges: any = [];
-    let t = c.series[String(lv)];
-    displayedBadges.push({
-      lv: lv,
-      name: c.name,
-      description: t.description,
-      icon_url: t.icon_url,
-      image_url: t.image_url,
-    });
-    displayedBadges.push(null);
-    delete c.id;
-    c.series = displayedBadges;
   }
 
-  return badges;
+  return [data, claimed_count];
 }
-
-async function loadUserBadgesCount(userId: string): Promise<any[]> {
-  console.log('loadUserBadgesCount');
-  const aggregateQuery: PipelineStage[] = [
-    {
-      $match: {
-        user_id: userId,
-      },
-    },
-    {
-      $project: {
-        badge_id: 1,
-      },
-    },
-  ];
-
-  const result = await UserBadges.aggregate(aggregateQuery);
-  return result;
-}
-
 // this will run if none of the above matches
 router.all((req, res) => {
   res.status(405).json({
