04157b9addedd63d9d5cfd46b73d965594bb8851
Eki <ichinoseeki@outlook.com>
2024-04-02 15:54:13 +0800

    chore: intro upgrade.

--- src/pages/components/home/EntertainmentSlide.tsx
+++ src/pages/components/home/EntertainmentSlide.tsx
@@ -8,11 +8,11 @@ interface Props {
 export default function EntertainmentSlide(props: Props) {
   return (
     <div className="bg-entertainment w-full h-screen relative flex justify-center items-center p-12">
-      <Image className="object-cover" src="/img/bg_home_swiper_entertainment.png" alt="" fill sizes="100%"></Image>
+      <Image className="object-cover" src="/img/bg_about.png" alt="" fill sizes="100%"></Image>
 
       <div className="absolute left-0 top-0 w-full h-full bg-black/40"></div>
 
-      <PageDesc
+      {/* <PageDesc
         goldenLogo
         hasBelt
         title={<div className='title text-6xl uppercase font-semakin mb-4 max-md:text-5xl'>Moonveil Entertainment</div>}
@@ -20,7 +20,7 @@ export default function EntertainmentSlide(props: Props) {
         baseAniTY
         className="relative top-[5%] items-center text-center w-full"
         subtitle={<div className='title text-lg font-decima mb-10 tracking-tighter max-w-[40rem]'>With the power of cutting-edge technologies, our mission is to craft top-notch gaming experiences that seamlessly combine casual flexibility with authentic fun depth.</div>}
-      />
+      /> */}
     </div>
   );
 }
