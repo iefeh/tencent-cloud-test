09a5207e3d6cde85bda8a3cbc457deebbf6e1846
Eki <ichinoseeki@outlook.com>
2024-04-09 10:43:20 +0800

    feat: add floats in foresight page.

--- src/pages/LoyaltyProgram/season/foresight.tsx
+++ src/pages/LoyaltyProgram/season/foresight.tsx
@@ -1,15 +1,16 @@
+import Astronaut from '@/components/LoyaltyProgram/season/Astronaut';
+import Planetoid from '@/components/LoyaltyProgram/season/Planetoid';
+import ShineBackground from '@/components/LoyaltyProgram/season/ShineBackground';
 import BGScreen from '@/components/LoyaltyProgram/season/noPass/BGScreen';
 import NoPassIndexScreen from '@/components/LoyaltyProgram/season/noPass/IndexScreen';
 import MainScreen from '@/components/LoyaltyProgram/season/noPass/MainScreen';
 import { useBattlePassContext } from '@/store/BattlePass';
 import { observer } from 'mobx-react-lite';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import { FC, useEffect } from 'react';
 
 const ForeSightPage: FC = () => {
-  const { info, init } = useBattlePassContext();
-  const router = useRouter();
+  const { init } = useBattlePassContext();
 
   useEffect(() => {
     init();
@@ -23,6 +24,14 @@ const ForeSightPage: FC = () => {
 
       <div className="relative w-full h-screen overflow-y-auto">
         <BGScreen />
+
+        <ShineBackground />
+
+        <div className="absolute inset-0 pointer-events-none">
+          <Astronaut className="!absolute right-[17.25rem] top-[42rem] w-[11.8125rem] h-[13.5625rem]" />
+
+          <Planetoid className="!absolute left-[11.5rem] top-[30.875rem] w-[5.4375rem] h-[7.9375rem]" />
+        </div>
 
         <NoPassIndexScreen />
 
