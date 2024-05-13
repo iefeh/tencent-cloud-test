05bc5075317e7eb71e1623f42b7a845f5c963ff4
Eki <ichinoseeki@outlook.com>
2024-05-13 13:16:51 +0800

    fix: delete temp userid.

--- src/lib/middleware/auth.ts
+++ src/lib/middleware/auth.ts
@@ -22,7 +22,6 @@ export async function mustAuthInterceptor(req: UserContextRequest, res: NextApiR
 
     // 将用户数据添加到请求对象中
     req.userId = userId; 
-    req.userId = 'faf5716d-439e-4680-98d1-577e6cab6edc';
 
     next();
 }
@@ -35,7 +34,6 @@ export async function maybeAuthInterceptor(req: UserContextRequest, res: NextApi
         if (userId) {
             // 将用户数据添加到请求对象中
             req.userId = userId;
-            req.userId = 'faf5716d-439e-4680-98d1-577e6cab6edc';
         }
     }
     next();
