444e1fe01df2680674fd143c52e89d059351843c
Eki <ichinoseeki@outlook.com>
2024-05-13 18:03:15 +0800

    fix: wallet not connected error.

--- src/pages/Profile/MyBadges/index.tsx
+++ src/pages/Profile/MyBadges/index.tsx
@@ -13,7 +13,8 @@ import { observer } from 'mobx-react-lite';
 import { MAX_DISPLAY_COUNT } from '@/constant/badge';
 import { isMobile } from 'react-device-detect';
 import BScroll from 'better-scroll';
-import MintSuccessModal from '@/components/profile/badges/components/MintSuccessModal';
+import MintSuccessModal from '@/components/profile/assets/MintSuccessModal';
+import MintConnectModal from '@/components/profile/assets/MintConnectModal';
 
 function MyBadgesPage() {
   const {
@@ -30,8 +31,24 @@ function MyBadgesPage() {
   const bsRef = useRef<BScroll | null>(null);
   const dbRef = useRef<DisplayBadgesRef>(null);
   const mintDisclosure = useDisclosure();
+  const connectDisclosure = useDisclosure();
+
+  async function getAccounts() {
+    try {
+      const accounts = (await window.ethereum.request({
+        method: 'eth_requestAccounts',
+        params: [],
+      })) as unknown as string[];
+      return accounts;
+    } catch (error) {
+      console.log('connect error:', error);
+    }
+  }
 
   async function onMint(id: string) {
+    const accounts = await getAccounts()
+    if (!accounts || accounts.length < 1) return;
+
     const res = await mintBadge(id);
     if (!res) return;
 
@@ -129,6 +146,8 @@ function MyBadgesPage() {
       />
 
       <MintSuccessModal disclosure={mintDisclosure} />
+
+      <MintConnectModal disclosure={connectDisclosure} onConnect={getAccounts} />
     </section>
   );
 }
