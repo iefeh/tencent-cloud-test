a437078604e2ff8a9610f6471354800679e390ba
Eki <ichinoseeki@outlook.com>
2024-05-13 10:54:03 +0800

    feat: category enum.

--- src/lib/models/Contract.ts
+++ src/lib/models/Contract.ts
@@ -4,7 +4,7 @@ import connectToMongoDbDev from "@/lib/mongodb/client";
 // 枚举合约分类，我们平台的合约类型，如SBT、DESTINY NFT等
 export enum ContractCategory {
     SBT = 'SBT',
-    TETRA_NFT = 'TEATRA_NFT',
+    TETRA_NFT = 'TETRA_SERIES',
 }
 
 // 子类型
