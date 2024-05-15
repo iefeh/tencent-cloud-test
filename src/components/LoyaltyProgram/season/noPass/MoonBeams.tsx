94f0422b2c08f35555625c4c0f555fef217ae64d
Eki <ichinoseeki@outlook.com>
2024-05-15 18:58:10 +0800

    fix: mb.

--- src/components/LoyaltyProgram/season/noPass/MoonBeams.tsx
+++ src/components/LoyaltyProgram/season/noPass/MoonBeams.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import earnMBsBgImg from 'img/profile/bg_earn_mbs.png';
 import mbImg from 'img/loyalty/earn/mb.png';
 import LGButton from '@/pages/components/common/buttons/LGButton';
-import MBHistoryButton from '@/pages/Profile/components/MBHistoryButton';
 import { useContext } from 'react';
 import { MobxContext } from '@/pages/_app';
 import { observer } from 'mobx-react-lite';
@@ -39,7 +38,8 @@ function MoonBeams() {
 
       <div className={cn(['flex flex-wrap gap-[0.8125rem] items-center relative z-0', isMobile && 'mt-4'])}>
         <LGButton label="Earn more Moon Beams >>" link="/LoyaltyProgram/earn" />
-        <MBHistoryButton />
+
+        <LGButton label="Moon Beams History >>" actived link="/Profile/mbhistory" />
       </div>
     </div>
   );
