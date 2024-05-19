910793e305241df775e637951de79297a0a7f472
Frank <36853880+iefeh@users.noreply.github.com>
2024-05-19 09:53:01 +0800

    Optimize code.
    Optimize code.

--- src/pages/api/oauth2/query_users.ts
+++ src/pages/api/oauth2/query_users.ts
@@ -15,7 +15,7 @@ router.use(dynamicCors).post(async (req, res) => {
     // 用于提供通过访问令牌获取用户信息的接口
     try {
         // 暂时取消授权
-        await OAuth2Server.authenticate(new Request(req), new Response(res), {scope: OAuth2Scopes.UserInfo})
+        // await OAuth2Server.authenticate(new Request(req), new Response(res), { scope: OAuth2Scopes.UserInfo })
         const wallets = req.body;
 
         if (!Array.isArray(wallets)) {
@@ -45,15 +45,15 @@ router.use(dynamicCors).post(async (req, res) => {
 
 async function queryUserInfo(wallets: any[]): Promise<any[]> {
     // 从user_wallet中查询出所有的user_id，和地址
-    let userWallets = await UserWallet.find({ deleted_time: null, wallet_addr: { $in: wallets } }, { user_id: 1, wallet_addr: 1 });
-    let userIds = userWallets.map(u => u.user_id);
-    let walletUserIdMap: Map<string, string> = new Map<string, string>(userWallets.map(u => [u.wallet_addr, u.user_id]));
+    let user_wallets = await UserWallet.find({ deleted_time: null, wallet_addr: { $in: wallets } }, { user_id: 1, wallet_addr: 1 });
+    let user_ids = user_wallets.map(u => u.user_id);
+    let wallet_user_id_map: Map<string, string> = new Map<string, string>(user_wallets.map(u => [u.wallet_addr, u.user_id]));
     // let userInfos = await User.find({ $or: [{ user_id: { $in: userIds } }, { particle: { evm_wallet: { $in: wallets } } }] }, { user_id: 1, username: 1, particle: 1 }); console.log("userInfos", userInfos);
     // 查询出所有用户信息
     const pipeline: PipelineStage[] = [
         {
             $match: {
-                $or: [{ user_id: { $in: userIds } }, { "particle.evm_wallet": { $in: wallets } }]
+                $or: [{ user_id: { $in: user_ids } }, { "particle.evm_wallet": { $in: wallets } }]
             }
         }, {
             $project: {
@@ -61,21 +61,25 @@ async function queryUserInfo(wallets: any[]): Promise<any[]> {
             }
         }
     ];
-    let userInfos = await User.aggregate(pipeline);
-    let particleUserInfoMap: Map<string, any> = new Map<string, any>(userInfos.map(u => [u.particle.evm_wallet, u]))
-    let userInfoMap: Map<string, any> = new Map<string, any>(userInfos.map(u => [u.user_id, u])); console.log("userInfoMap", userInfoMap);
-    
+    let user_infos = await User.aggregate(pipeline);
+    let particle_user_info_map: Map<string, any> = new Map<string, any>(user_infos.map(u => [u.particle.evm_wallet, u]))
+    let user_info_map: Map<string, any> = new Map<string, any>(user_infos.map(u => [u.user_id, u])); console.log("userInfoMap", user_info_map);
+
 
     let result: any[] = [];
     for (let w of wallets) {
-        if (walletUserIdMap.has(w)) {
-            result.push({ user_id: walletUserIdMap.get(w), username: userInfoMap.get(walletUserIdMap.get(w)!).username, address: w });
+        if (wallet_user_id_map.has(w)) {
+            let user_data: any = {};
+            user_data.user_id = wallet_user_id_map.get(w);
+            user_data.usernmae = user_info_map.get(user_data.user_id).username;
+            user_data.address = w;
+            result.push(user_data);
         }
-        if (particleUserInfoMap.has(w)) {
-            result.push({ user_id: particleUserInfoMap.get(w).user_id, username: particleUserInfoMap.get(w).username, address: w });
+        if (particle_user_info_map.has(w)) {
+            result.push({ user_id: particle_user_info_map.get(w).user_id, username: particle_user_info_map.get(w).username, address: w });
         }
     }
-  
+
     return result;
 }
 // this will run if none of the above matches
