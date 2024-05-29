fdbc9e751abc5d3f55dde828d8604da31d7e7f8d
Eki <ichinoseeki@outlook.com>
2024-05-29 10:44:07 +0800

    feat: hide entry.

--- src/pages/components/common/Header/index.tsx
+++ src/pages/components/common/Header/index.tsx
@@ -56,10 +56,10 @@ const routeText: RouteMenu[] = [
         name: 'Referral Program',
         route: '/Profile/invite',
       },
-      {
-        name: '“More and $MORE” Lottery',
-        route: '/lottery',
-      },
+      // {
+      //   name: '“More and $MORE” Lottery',
+      //   route: '/lottery',
+      // },
     ],
   },
   {
