940f7c9b3e3a69f7cb2a0febd6ae66998a92ec8c
Eki <ichinoseeki@outlook.com>
2024-06-04 11:18:46 +0800

    feat: split com of media item.

---
+++ src/components/pages/profile/edit/MediaItem.tsx
@@ -0,0 +1,55 @@
+import { MediaType } from '@/constant/task';
+import useConnect from '@/hooks/useConnect';
+import { useUserContext } from '@/store/User';
+import Image, { StaticImageData } from 'next/image';
+import { FC } from 'react';
+import rightArrowIconImg from 'img/profile/edit/icon_arrow_right.png';
+
+export interface MAItem {
+  title: string;
+  icon: string | StaticImageData;
+  type: MediaType;
+  format?: (val?: string) => string;
+  connected?: boolean;
+  connectedAccount?: string;
+  disconnectAPI?: () => Promise<boolean | null>;
+}
+
+interface Props {
+  item: MAItem;
+  onDisconnectClick?: (item: MAItem) => void;
+}
+
+const MediaItem: FC<Props> = ({ item, onDisconnectClick }) => {
+  const { getUserInfo } = useUserContext();
+  const { onConnect, BindTipsModal } = useConnect(item.type, () => {
+    getUserInfo();
+  });
+
+  return (
+    <div className="flex items-center py-[1.0625rem] pl-[1.6875rem] pr-[1.875rem] border-1 border-[#252525] rounded-base bg-black hover:border-basic-yellow transition-[border-color] !duration-500">
+      <Image className="w-[1.625rem] h-[1.625rem] object-contain" src={item.icon} alt="" width={26} height={26} />
+
+      <div className="ml-4 flex-1 font-poppins-medium text-base">{item.title}</div>
+
+      <div className="cursor-pointer">
+        {item.connected ? (
+          <span className="relative group" onClick={() => onDisconnectClick?.(item)}>
+            <span className="group-hover:text-transparent transition-colors inline-block max-w-[16rem] overflow-hidden whitespace-nowrap text-ellipsis">
+              {(item.format ? item.format(item.connectedAccount) : item.connectedAccount) || 'Connected'}
+            </span>
+            <span className="absolute right-0 z-0 text-transparent group-hover:bg-black group-hover:text-basic-yellow transition-all !duration-500">
+              Disconnect
+            </span>
+          </span>
+        ) : (
+          <Image className="w-[1.375rem] h-4" src={rightArrowIconImg} alt="" onClick={onConnect} />
+        )}
+      </div>
+
+      <BindTipsModal />
+    </div>
+  );
+};
+
+export default MediaItem;
