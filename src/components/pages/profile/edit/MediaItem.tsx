8fdbec9d3955f1b05fa149d065e9b972beac5564
Eki <ichinoseeki@outlook.com>
2024-06-04 11:25:26 +0800

    feat: add tooltip on media accounts.

--- src/components/pages/profile/edit/MediaItem.tsx
+++ src/components/pages/profile/edit/MediaItem.tsx
@@ -4,6 +4,8 @@ import { useUserContext } from '@/store/User';
 import Image, { StaticImageData } from 'next/image';
 import { FC } from 'react';
 import rightArrowIconImg from 'img/profile/edit/icon_arrow_right.png';
+import { Tooltip } from '@nextui-org/react';
+import errorIconImg from 'img/icon/icon_error.png';
 
 export interface MAItem {
   title: string;
@@ -30,7 +32,24 @@ const MediaItem: FC<Props> = ({ item, onDisconnectClick }) => {
     <div className="flex items-center py-[1.0625rem] pl-[1.6875rem] pr-[1.875rem] border-1 border-[#252525] rounded-base bg-black hover:border-basic-yellow transition-[border-color] !duration-500">
       <Image className="w-[1.625rem] h-[1.625rem] object-contain" src={item.icon} alt="" width={26} height={26} />
 
-      <div className="ml-4 flex-1 font-poppins-medium text-base">{item.title}</div>
+      <Tooltip
+        content={
+          <div className="max-w-[28rem] px-4 py-2 flex">
+            <Image className="w-6 h-6 object-contain align-bottom mr-2" src={errorIconImg} alt="" />
+
+            <div>
+              We recommend all Moonwalkers to connect your{' '}
+              <span className="text-basic-yellow">commonly used accounts</span>. The{' '}
+              <span className="text-basic-yellow">rewards</span> and{' '}
+              <span className="text-basic-yellow">game data</span> in the Moonveil ecosystem will be tightly linked to
+              the connected account. Please note that frequent disconnection and reconnection{' '}
+              <span className="text-basic-yellow">may result in the inability to claim rewards</span>.
+            </div>
+          </div>
+        }
+      >
+        <div className="ml-4 flex-1 font-poppins-medium text-base">{item.title}</div>
+      </Tooltip>
 
       <div className="cursor-pointer">
         {item.connected ? (
