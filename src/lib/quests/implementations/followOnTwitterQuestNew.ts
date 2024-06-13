a52d2389d149571d3ea6fa179780adce37e39242
Frank <36853880+iefeh@users.noreply.github.com>
2024-06-13 23:50:42 +0800

    Update followOnTwitterQuestNew.ts

--- src/lib/quests/implementations/followOnTwitterQuestNew.ts
+++ src/lib/quests/implementations/followOnTwitterQuestNew.ts
@@ -1,6 +1,5 @@
 import { IQuest } from '@/lib/models/Quest';
 import { FollowOnTwitter, checkClaimableResult, claimRewardResult } from '@/lib/quests/types';
-import UserTwitter from '@/lib/models/UserTwitter';
 import { AuthorizationType } from '@/lib/authorization/types';
 import { QuestBase } from './base';
 import UserMetrics, { Metric } from '@/lib/models/UserMetrics';
@@ -12,15 +11,10 @@ import logger from "@/lib/logger/winstonLogger";
 import { twitterOAuthProvider } from "@/lib/authorization/provider/twitter";
 import { isAxiosError } from "axios";
 import { deleteAuthToken } from "@/lib/authorization/provider/util";
-import userinfo from '@/pages/api/oauth2/userinfo';
+import { FollowOnTwitterQuest } from './followOnTwitterQuest';
 
-export class FollowOnTwitterQuest extends QuestBase {
-  // 定义twitter账号与指标的映射关系
-  private readonly followMetrics = new Map<string, Metric>([
-    [process.env.MOONVEIL_TWITTER_USERNAME!, Metric.TwitterFollowedMoonveil],
-    [process.env.ASTRARK_TWITTER_USERNAME!, Metric.TwitterFollowedAstrArk],
-  ]);
-
+export class FollowOnTwitterQuestNew extends FollowOnTwitterQuest {
+  
   constructor(quest: IQuest) {
     super(quest);
   }
@@ -54,7 +48,7 @@ export class FollowOnTwitterQuest extends QuestBase {
     try {
 
       // 直接发送关注用户的请求
-      const data: any = await twitterRequest.post(queryFollowerURL, { target_user_id: questProp.target_twitter_id });console.log(data);
+      const data: any = await twitterRequest.post(queryFollowerURL, { target_user_id: questProp.target_twitter_id }); console.log(data);
 
       // 校验结果
       const claimable: boolean = data.data.following;
@@ -96,70 +90,5 @@ export class FollowOnTwitterQuest extends QuestBase {
       }
       throw error;
     }
-  }
-
-  private checkUserMetric(userId: string): undefined | ((session: ClientSession) => Promise<void>) {
-    const questProp = this.quest.properties as FollowOnTwitter;
-    const followMetric = this.followMetrics.get(questProp.username);
-    let updateMetric = undefined;
-    if (followMetric) {
-      updateMetric = async (session: any) => {
-        await UserMetrics.updateOne(
-          { user_id: userId },
-          {
-            $set: {
-              [followMetric]: 1,
-              [Metric.TwitterConnected]: 1,
-            },
-            $setOnInsert: {
-              created_time: Date.now(),
-            },
-          },
-          { upsert: true, session: session },
-        );
-      };
-    }
-    return updateMetric;
-  }
-
-  private async sendBadgeCheckMessage(userId: string) {
-    const questProp = this.quest.properties as FollowOnTwitter;
-    const followMetric = this.followMetrics.get(questProp.username);
-    if (followMetric) {
-      await sendBadgeCheckMessages(userId, {
-        [followMetric]: 1,
-        [Metric.TwitterConnected]: 1,
-      });
-    }
-  }
-
-  async claimReward(userId: string): Promise<claimRewardResult> {
-    const claim = await this.checkClaimable(userId);
-    if (!claim.claimable) {
-      return {
-        verified: false,
-        tip: 'Please click follow to complete task first.',
-      };
-    }
-    // 检查是否触发关注指标
-    const updateMetric = this.checkUserMetric(userId);
-
-    // 污染twitter，确保同一个twitter单任务只能获取一次奖励
-    const taint = `${this.quest.id},${AuthorizationType.Twitter},${claim.extra}`;
-    const rewardDelta = await this.checkUserRewardDelta(userId);
-    const result = await this.saveUserReward(userId, taint, rewardDelta, null, updateMetric);
-    if (result.duplicated) {
-      return {
-        verified: false,
-        tip: 'The Twitter Account has already claimed reward.',
-      };
-    }
-    // 检查当前满足的指标并发送消息
-    await this.sendBadgeCheckMessage(userId);
-    return {
-      verified: result.done,
-      claimed_amount: result.done ? rewardDelta : undefined,
-      tip: result.done ? `You have claimed ${rewardDelta} MB.` : 'Server Internal Error',
-    };
   }
 }
