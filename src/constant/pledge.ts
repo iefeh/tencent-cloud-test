f331f836d888e45764e6c9ba73c51304eacdf00c
Eki <ichinoseeki@outlook.com>
2024-07-01 15:11:30 +0800

    feat: icons.

--- src/constant/pledge.ts
+++ src/constant/pledge.ts
@@ -7,11 +7,11 @@ export const enum PoolType {
 export const PoolProps = {
   [PoolType.USDT]: {
     id: -1,
-    icon: 'https://moonveil-public.s3.ap-southeast-2.amazonaws.com/pledge/icons/icon_eth.png',
+    icon: 'https://moonveil-public.s3.ap-southeast-2.amazonaws.com/pledge/icons/icon_usdt.png',
   },
   [PoolType.USDC]: {
     id: 1,
-    icon: 'https://moonveil-public.s3.ap-southeast-2.amazonaws.com/pledge/icons/icon_eth.png',
+    icon: 'https://moonveil-public.s3.ap-southeast-2.amazonaws.com/pledge/icons/icon_usdc.png',
   },
   [PoolType.ETH]: {
     id: 0,
