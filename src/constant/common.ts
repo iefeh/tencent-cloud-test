7f63be87ee84eeb874776a1f6592f06d8c3d0e33
Eki <ichinoseeki@outlook.com>
2024-08-06 11:57:06 +0800

    feat: guidelines.

--- src/constant/common.ts
+++ src/constant/common.ts
@@ -1,3 +1,11 @@
 export const BASE_CLIENT_WIDTH = 1920;
 export const BASE_CLIENT_HEIGHT = 1080;
 export const URL_OPENSEA = 'https://opensea.io/collection/destiny-tetra';
+
+export const MediaURLs = {
+  DISCORD: 'https://discord.gg/moonveil',
+  X: 'https://twitter.com/Moonveil_Studio',
+  TELEGRAM: 'https://t.me/+AeiqS8o2YmswYTgx',
+  MEDIUM: 'https://medium.com/@Moonveil_Studio',
+  YOUTUBE: 'https://www.youtube.com/channel/UCFtFhgsjtdSgXarKvSYpz3A',
+};
