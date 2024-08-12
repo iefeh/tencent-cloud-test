188a094fc923d5cb349e17a2c8a078d881cb7aa6
Eki <ichinoseeki@outlook.com>
2024-08-12 11:08:26 +0800

    feat: abstract transaction class.

--- src/components/profile/MyTokens/useClaimToken.ts
+++ src/components/profile/MyTokens/useClaimToken.ts
@@ -1,16 +1,25 @@
+import useTransaction from '@/hooks/wallet/useTransaction';
 import { queryTokenPermitAPI, type MyTokensRecord } from '@/http/services/token';
+import tokenRewardABI from '@/http/abi/tokenReward.json';
+import { useState } from 'react';
 
 export default function useClaimToken() {
+  const [loading, setLoading] = useState(false);
+  const { loading: transactionLoading, onTransaction } = useTransaction({ abi: tokenRewardABI, method: 'ClaimToken' });
+
   async function onClaim(item: MyTokensRecord) {
-    // TODO token领取逻辑
+    setLoading(true);
     const permitRes = await queryTokenPermitAPI({ reward_ids: item.reward_id });
-    // 0. 判断用户是否已绑定钱包地址，否 -> 弹窗提示钱包没有绑定
-    // 1. 根据 item.reward_id 获取permit
-    // 2. 调用abi方法
-    // 2.1 确认钱包环境：连接钱包、网络是否匹配
-    // 2.2 通过合约地址调用合约方法
-    // 3. 回传transaction hash
+    if (!permitRes) {
+      setLoading(false);
+      return;
+    }
+
+    const { contract_address, chain_id, permits } = permitRes;
+    const res = await onTransaction(permits[0], { contractAddress: contract_address, chainId: chain_id });
+    console.log('claim result:', res);
+    setLoading(false);
   }
 
-  return { onClaim };
+  return { loading, transactionLoading, onClaim };
 }
