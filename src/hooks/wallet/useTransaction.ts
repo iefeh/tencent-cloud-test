49f28cc16ef7f2fb4d2f77fb46e20895e4c311f8
cyberjill <1030639552@qq.com>
2024-08-14 16:39:21 +0800

    fix: remove beready  function

--- src/hooks/wallet/useTransaction.ts
+++ src/hooks/wallet/useTransaction.ts
@@ -31,7 +31,6 @@ export default function useTransaction({ abi, method }: Props) {
     if (!txProvider.current) return;
 
     setLoading(true);
-    await txProvider.current.beReady();
     const res = await txProvider.current.transaction(params, options, config);
     setLoading(false);
     return res;
