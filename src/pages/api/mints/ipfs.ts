f90711a036ad3286180b023578bd5c7886dfc0f9
heyuanyou <harrell16805@163.com>
2024-08-16 17:26:47 +0800

    Update ipfs.ts

--- src/pages/api/mints/ipfs.ts
+++ src/pages/api/mints/ipfs.ts
@@ -22,6 +22,7 @@ router.get(async (req, res) => {
     if (!badge_id || !level) {
         return res.json(response.invalidParams());
     }
+    
     const ipfsHash = await getBadgeSeriesMetadataHash(badge_id as string, level as string);
     return res.json(response.success({
         ipfs_hash: ipfsHash
