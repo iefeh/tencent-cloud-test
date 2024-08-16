c32e1c4a84effb4ea466083d088662958b994c92
Eki <ichinoseeki@outlook.com>
2024-08-16 14:16:28 +0800

    feat: add transaction class & hook.

---
+++ src/hooks/wallet/useTransaction.ts
@@ -0,0 +1,58 @@
+import { useUserContext } from '@/store/User';
+import TransactionProvider, { type TransactionConfig } from '@/utils/wallet/transaction';
+import { useWeb3Modal, useWeb3ModalAccount, useWeb3ModalProvider } from '@web3modal/ethers/react';
+import { useEffect, useRef, useState } from 'react';
+import { type TransactionRequest } from 'ethers';
+import { toast } from 'react-toastify';
+
+interface Props {
+  abi: any;
+  method: string;
+}
+
+export default function useTransaction({ abi, method }: Props) {
+  const { userInfo, toggleLoginModal } = useUserContext();
+  const { walletProvider } = useWeb3ModalProvider();
+  const { isConnected } = useWeb3ModalAccount();
+  const { open } = useWeb3Modal();
+  const [loading, setLoading] = useState(false);
+  const txProvider = useRef<TransactionProvider>();
+
+  async function onTransaction(params: any, options: TransactionRequest = {}, config: Partial<TransactionConfig>) {
+    if (!userInfo) {
+      toggleLoginModal(true);
+      return;
+    }
+
+    if (!isConnected) {
+      open();
+      return;
+    }
+
+    if (!txProvider.current) return;
+
+    setLoading(true);
+    const res = await txProvider.current.transaction(params, options, config);
+    if (!res) {
+      toast.error('Transaction failed, please try again later.');
+    }
+    setLoading(false);
+    return res;
+  }
+
+  useEffect(() => {
+    if (isConnected && walletProvider) {
+      txProvider.current = new TransactionProvider({
+        provider: walletProvider,
+        config: {
+          abi,
+          method,
+        },
+      });
+    } else {
+      txProvider.current = undefined;
+    }
+  }, [walletProvider, isConnected]);
+
+  return { loading, onTransaction };
+}
