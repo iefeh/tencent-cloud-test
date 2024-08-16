5ed70f8f21ea02766d6d3165abfab681c1c1b0a6
kalos <icymoon0322@gmail.com>
2024-08-16 16:48:58 +0800

    add properties and pagination to lottery list api

--- src/pages/api/lottery/lotterypool.ts
+++ src/pages/api/lottery/lotterypool.ts
@@ -1,30 +1,34 @@
 import type {NextApiResponse} from "next";
 import { createRouter } from 'next-connect';
 
+import {
+    canClaimPremiumBenifits, enrichRequirementsInfo, getActiveLotteryPoolById,
+    lotteryRequirementSatisfy
+} from '@/lib/lottery/lottery';
 import { mustAuthInterceptor, UserContextRequest } from '@/lib/middleware/auth';
 import { errorInterceptor } from '@/lib/middleware/error';
 import { ILotteryPool } from '@/lib/models/LotteryPool';
+import LotteryPoolRequirement from '@/lib/models/LotteryPoolRequirements';
 import User from '@/lib/models/User';
 import UserLotteryPool from '@/lib/models/UserLotteryPool';
 import * as response from '@/lib/response/response';
-import { canClaimPremiumBenifits, getLotteryPoolById } from '@/lib/lottery/lottery';
 
 const router = createRouter<UserContextRequest, NextApiResponse>();
 router.use(errorInterceptor(), mustAuthInterceptor).get(async (req, res) => {
   const { lottery_pool_id } = req.query;
   if (!lottery_pool_id) {
-    res.json(response.invalidParams());
-    return;
+    return res.json(response.invalidParams());
   }
   const userId = String(req.userId);
   const lotteryPoolId = String(lottery_pool_id);
-  const lotteryPool = await getLotteryPoolById(lotteryPoolId) as ILotteryPool;
+  const lotteryPool = await getActiveLotteryPoolById(lotteryPoolId) as ILotteryPool;
   if (!lotteryPool) {
-    res.json(response.invalidParams("The lottery pool is not opened or has been closed."));
-    return;
+    return res.json(response.invalidParams({ message: "The lottery pool is not opened or has been closed." }));
   }
   const user = await User.findOne({ user_id: userId });
   const userLotteryPool = await UserLotteryPool.findOne({ user_id: userId, lottery_pool_id: lotteryPoolId, deleted_time: null });
+  const requirements = await LotteryPoolRequirement.find({ lottery_pool_id: lotteryPoolId }, { type: 1, description: 1, properties: 1, _id: 0 });
+  await enrichRequirementsInfo(requirements);
   const notifiyPremiumBenifitsClaim = await canClaimPremiumBenifits(userId, lotteryPoolId);
   let restDrawAmount: string | number = 0;
   if (lotteryPool.draw_limits === null) {
@@ -35,6 +39,7 @@ router.use(errorInterceptor(), mustAuthInterceptor).get(async (req, res) => {
   }
   const userFreeLotteryTicketAmount = userLotteryPool ? userLotteryPool.free_lottery_ticket_amount : 0;
   const firstTwitterTopicVerified = userLotteryPool? userLotteryPool.first_twitter_topic_verified: false;
+  const requirementSatisfy = await lotteryRequirementSatisfy(userId, lotteryPoolId); 
   let rewards: any[] = [];
   for (let reward of lotteryPool.rewards) {
     rewards.push({
@@ -48,16 +53,22 @@ router.use(errorInterceptor(), mustAuthInterceptor).get(async (req, res) => {
   }
   res.json(response.success({ 
     lottery_pool_id: lotteryPool.lottery_pool_id,
+    icon_url: lotteryPool.icon_url,
+    name: lotteryPool.name,
     title: lotteryPool.title,
     start_time: lotteryPool.start_time,
     end_time: lotteryPool.end_time,
     draw_limits: lotteryPool.draw_limits,
+    limited_qty: lotteryPool.limited_qty,
     rest_draw_amount: restDrawAmount,
     user_s1_lottery_ticket_amount: user.lottery_ticket_amount,
     user_free_lottery_ticket_amount: userFreeLotteryTicketAmount,
     user_mb_amount: user.moon_beam,
+    user_meet_requirement_type: requirementSatisfy.requirement_type,
+    user_meet_requirement: requirementSatisfy.meet_requirement,
     can_claim_premium_benifits: notifiyPremiumBenifitsClaim,
     first_twitter_topic_verified: firstTwitterTopicVerified,
+    requirements: requirements,
     rewards: rewards
   }));
   return;
