ac910d0159ddbf8f1f660c7aad6ef58bd797a44d
Eki <ichinoseeki@outlook.com>
2024-08-16 14:56:08 +0800

    feat: error in sbt.

--- src/hooks/useSbtMint.ts
+++ src/hooks/useSbtMint.ts
@@ -1,12 +1,22 @@
 import { MintPermitResDTO } from '@/http/services/badges';
 import sbtContractABI from '@/http/sbt_abi.json';
 import useTransaction from './wallet/useTransaction';
+import { toast } from 'react-toastify';
 
 export default function useSbtMint() {
   const { loading, onTransaction } = useTransaction({ abi: sbtContractABI, method: 'mint' });
 
   async function onButtonClick(data: MintPermitResDTO) {
-    const result = await onTransaction(data.permit, { chainId: data.chain_id, contractAddress: data.contract_address });
+    const result = await onTransaction({
+      params: data.permit,
+      config: { chainId: data.chain_id, contractAddress: data.contract_address },
+      onError: (code, message = '') => {
+        if (message.indexOf('permit already used')) {
+          toast.error('You have already minted SBT. Please wait for data confirmation patiently.');
+          return true;
+        }
+      },
+    });
 
     return !!result;
   }
