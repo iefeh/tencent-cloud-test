45855bf5d981d942fa7409ef2edae938ee59c872
cyberjill <1030639552@qq.com>
2024-08-26 11:22:04 +0800

    fix: speed adjustment

--- src/pages/minigames/index.tsx
+++ src/pages/minigames/index.tsx
@@ -10,6 +10,7 @@ import { FC, useState, CSSProperties } from 'react';
 
 import 'swiper/css';
 
+export const swiperSpeed = 1500;
 
 const MiniGamesPage: FC & BasePage = () => {
   const [selectedKey, setSelectedKey] = useState('');
@@ -37,7 +38,7 @@ const MiniGamesPage: FC & BasePage = () => {
         modules={[Mousewheel, FreeMode]}
         mousewheel={true}
         direction="vertical"
-        speed={1300}
+        speed={swiperSpeed}
         scrollbar={{ draggable: true }}
         slidesPerView="auto"
         spaceBetween={0}
