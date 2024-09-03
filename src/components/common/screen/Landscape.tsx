7017f3328602f57ee26bed5b1556630ecf44dfcf
Eki <ichinoseeki@outlook.com>
2024-09-03 17:14:34 +0800

    feat: add landscape mode.

---
+++ src/components/common/screen/Landscape.tsx
@@ -0,0 +1,26 @@
+import { type FC, type PropsWithChildren, useEffect, useState } from 'react';
+
+const Landscape: FC<PropsWithChildren> = ({ children }) => {
+  const [isHorizontal, setIsHorizontal] = useState(false);
+
+  function onResize() {
+    const { innerWidth, innerHeight } = window;
+    setIsHorizontal(innerWidth > innerHeight);
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', onResize);
+
+    return () => window.removeEventListener('resize', onResize);
+  }, []);
+
+  if (isHorizontal) return children;
+
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <span className="text-2xl">Please view this page in landscape mode.</span>
+    </div>
+  );
+};
+
+export default Landscape;
--- src/pages/AstrArk/shop/index.tsx
+++ src/pages/AstrArk/shop/index.tsx
@@ -1,10 +1,11 @@
-import AstrArkLayout from "@/components/layouts/AstrArkLayout";
-import { observer } from "mobx-react-lite";
-import { NextPage } from "next"
+import Landscape from '@/components/common/screen/Landscape';
+import AstrArkLayout from '@/components/layouts/AstrArkLayout';
+import { observer } from 'mobx-react-lite';
+import { NextPage } from 'next';
 
 const DeleteAccountPage: NextPage & BasePage = () => {
-  return <div></div>
-}
+  return <Landscape></Landscape>;
+};
 
 DeleteAccountPage.getLayout = AstrArkLayout;
 
