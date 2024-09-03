4a4309fc4bb6c15e6ec8ae6aa014495e17ae0ab9
Eki <ichinoseeki@outlook.com>
2024-09-03 11:26:05 +0800

    feat: add page.

---
+++ src/pages/AstrArk/shop/index.tsx
@@ -0,0 +1,11 @@
+import AstrArkLayout from "@/components/layouts/AstrArkLayout";
+import { observer } from "mobx-react-lite";
+import { NextPage } from "next"
+
+const DeleteAccountPage: NextPage & BasePage = () => {
+  return <div></div>
+}
+
+DeleteAccountPage.getLayout = AstrArkLayout;
+
+export default observer(DeleteAccountPage);
