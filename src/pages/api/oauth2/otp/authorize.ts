f6a79299e8830be0d1f059e648a741aebcb4f578
kalos <icymoon0322@gmail.com>
2024-09-04 13:39:14 +0800

    rename and update responses

--- src/pages/api/oauth2/otp/authorize.ts
+++ src/pages/api/oauth2/otp/authorize.ts
@@ -3,7 +3,7 @@ import { createRouter } from 'next-connect';
 import { v4 as uuidV4 } from 'uuid';
 
 import { UserContextRequest, dynamicCors } from "@/lib/middleware/auth";
-import OAuth2GameCode from '@/lib/models/OAuth2GameCode';
+import OAuth2ExchangeCode from '@/lib/models/OAuth2ExchangeCode';
 import { OAuth2Scopes } from '@/lib/models/OAuth2Scopes';
 import { responseOnOauthError } from '@/lib/oauth2/response';
 import OAuth2Server from '@/lib/oauth2/server';
@@ -16,22 +16,22 @@ router.use(dynamicCors).post(async (req, res) => {
     try {
         const token = await OAuth2Server.authenticate(new Request(req), new Response(res), { scope: OAuth2Scopes.UserInfo });
         const userId = token.user.user_id;
-        const lockKey = `gameToken,${userId}`;
+        const lockKey = `exchangeCode,${userId}`;
         const locked = await redis.set(lockKey, Date.now(), "EX", 10, "NX");
         if (!locked) {
             return res.json(response.tooManyRequests());
         }
-        await OAuth2GameCode.deleteOne({ user_id: userId, client_id: token.client.id });
-        const gameToken = new OAuth2GameCode({
+        await OAuth2ExchangeCode.deleteOne({ user_id: userId, client_id: token.client.id });
+        const exchangeCode = new OAuth2ExchangeCode({
             user_id: userId,
             client_id: token.client.id,
             authorization_code: uuidV4(),
-            expires_at: Date.now() + 24 * 60 * 60 * 1000,
+            expires_at: Date.now() + 10 * 60 * 1000,
         });
-        await gameToken.save();
+        await exchangeCode.save();
         res.json(response.success({
-            authorization_code: gameToken.authorization_code,
-            expires_at: gameToken.expires_at
+            authorization_code: exchangeCode.authorization_code,
+            expires_at: exchangeCode.expires_at
         }));
     } catch (error: any) {
         return responseOnOauthError(res, error);
--- src/pages/api/oauth2/otp/exchange.ts
+++ src/pages/api/oauth2/otp/exchange.ts
@@ -3,21 +3,21 @@ import { createRouter } from 'next-connect';
 
 import { UserContextRequest } from '@/lib/middleware/auth';
 import { errorInterceptor } from '@/lib/middleware/error';
-import OAuth2GameCode from '@/lib/models/OAuth2GameCode';
+import OAuth2ExchangeCode from '@/lib/models/OAuth2ExchangeCode';
 import OAuth2Token from '@/lib/models/OAuth2Token';
 import * as response from '@/lib/response/response';
 
 const router = createRouter<UserContextRequest, NextApiResponse>();
 router.use(errorInterceptor()).post(async (req, res) => {
     const { authorization_code } = req.body;
-    const code = await OAuth2GameCode.findOne({ authorization_code: authorization_code, expires_at: { $gt: Date.now()}});
+    const code = await OAuth2ExchangeCode.findOne({ authorization_code: authorization_code, expires_at: { $gt: Date.now()}});
     if (!code) {
-        return res.json(response.unauthorized());
+        return res.json(response.authorizationCodeExpired());
     }
     const token = await OAuth2Token.findOne({ user_id: code.user_id, client_id: code.client_id, access_token_expires_at: { $gt: Date.now() }});
-    await OAuth2GameCode.deleteOne({ authorization_code: authorization_code });
+    await OAuth2ExchangeCode.deleteOne({ authorization_code: authorization_code });
     if (!token) {
-        return res.json(response.unauthorized());
+        return res.json(response.authorizationCodeExpired());
     }
     return res.json(response.success({
         access_token: token.access_token,
