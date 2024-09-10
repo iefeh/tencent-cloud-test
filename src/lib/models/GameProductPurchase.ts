e2b5de97fa2fc2bcb5603624898f12f2412381c3
heyuanyou <harrell16805@163.com>
2024-09-10 11:50:35 +0800

    add permit

--- src/lib/models/GameProductPurchase.ts
+++ src/lib/models/GameProductPurchase.ts
@@ -2,6 +2,8 @@ import {Document, Schema, models, model} from 'mongoose'
 import connectToMongoDbDev from "@/lib/mongodb/client";
 
 export interface IGameProductPurchase extends Document {
+    // 请求id
+    request_id: string;
     // 用户id
     user_id: string;
     // 游戏id
@@ -10,13 +12,17 @@ export interface IGameProductPurchase extends Document {
     product_id: string;
     // 购买时间, ISO时间戳, daily: 
     purchase_period: string;
+    // 通知时间, 毫秒时间戳，什么时候通知游戏侧并且对方响应http 200状态码.
+    notify_time: number;
 }
 
 const GameProductPurchaseSchema = new Schema<IGameProductPurchase>({
+    request_id: { type: String, required: true },
     user_id: { type: String, required: true },
     game_id: { type: String, required: true },
     product_id: { type: String, required: true },
     purchase_period: { type: String, required: true },
+    notify_time: { type: Number },
 });
 
 GameProductPurchaseSchema.index({ product_id: 1 });
