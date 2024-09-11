e7630e49666a5eec90997c5f7315716067a53268
Eki <ichinoseeki@outlook.com>
2024-09-11 13:51:52 +0800

    feat: countdown.

--- src/components/LoyaltyProgram/task/TokenRewardProgress.tsx
+++ src/components/LoyaltyProgram/task/TokenRewardProgress.tsx
@@ -87,8 +87,7 @@ function TokenRewardProgress({ task, status }: Props) {
             endTime={status < 2 ? task.participant_end_time : task.end_time}
           />
         ) : (
-          status !== 1 &&
-          status < 3 && (
+          status !== 1 && (
             <TokenRewardProgressCountdown
               label={getProgressLabel(status)}
               endTime={status === 0 ? item?.estimated_raffle_time : task.end_time}
