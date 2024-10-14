446e0901e3c078dd033a0961318fcf2fb9de0f11
cyberjill <zhiweir@moonveil.studio>
2024-10-14 14:18:21 +0800

    feat: first screen ani

--- src/components/pages/gyoza/sliders/FlamingHome.tsx
+++ src/components/pages/gyoza/sliders/FlamingHome.tsx
@@ -1,28 +1,75 @@
-import React, { FC } from "react";
+import React, { FC, CSSProperties } from "react";
 import S3Image from '@/components/common/medias/S3Image';
 import { cn } from "@nextui-org/react";
+import useSwiperIndex, { type SwiperIndexProps, SliderAnimationType } from "../hooks/useSwiperIndex";
+
+interface FlamingHomeProps extends SwiperIndexProps {
+
+}
 
 const getUrl = (name: string) => {
   return `/minigames/miner/${name}.png`
 }
 
+const aniTime = 1.5
+
 const imgUrls = [
-  { url: getUrl('title'), classNames: 'w-[34.625rem] h-[15.375rem] top-[16.125rem] right-[50%] translate-x-1/2' },
-  { url: getUrl('subtitle'), classNames: 'w-[54rem] h-[10.9375rem] top-[33.1875rem] right-[50%] translate-x-1/2' },
+  {
+    url: getUrl('title'),
+    classNames: 'w-[34.625rem] h-[15.375rem] top-[16.125rem] right-[50%] translate-x-1/2',
+    outStyles: {},
+  },
+  {
+    url: getUrl('subtitle'),
+    classNames: 'w-[54rem] h-[10.9375rem] top-[33.1875rem] right-[50%] translate-x-1/2',
+    styles: {}
+  },
 ]
 
 const videoUrls = [
-  { url: '/video/boat.webm', poster: getUrl('cannon'), classNames: 'w-full h-full top-[-2rem] right-0' },
-  { url: '/video/cat.webm', poster: getUrl('castle'), classNames: 'w-full h-full bottom-0 left-0' },
+  {
+    url: '/video/boat.webm',
+    poster: getUrl('cannon'),
+    classNames: 'w-full h-full top-[-2rem] right-[-7rem]',
+    styles: {
+      '--translate-x': '100%',
+      '--translate-y': '0',
+      '--duration-time': `${aniTime}s`
+    },
+  },
+  {
+    url: '/video/cat.webm',
+    poster: getUrl('castle'),
+    classNames: 'w-full h-full bottom-0 left-[-7rem]',
+    styles: {
+      '--translate-x': '-100%',
+      '--translate-y': '0',
+      '--duration-time': `${aniTime}s`
+    }
+  },
 ]
 
-const FlamingHome: FC = () => {
+const FlamingHome: FC<FlamingHomeProps> = (props) => {
+  const { aimIndex, prevIndex, sliderIndex } = props || {}
+
+  const { aniSts } = useSwiperIndex({
+    aimIndex,
+    prevIndex,
+    sliderIndex,
+  })
+
   return (
     <div>
       {videoUrls.map((video, index) => (
         <video
           key={index}
-          className={'absolute object-contain' + video.classNames}
+          style={video.styles as CSSProperties}
+          className={cn([
+            'absolute object-contain',
+            aniSts === SliderAnimationType.FADE_IN && 'ani-fade-in',
+            aniSts === SliderAnimationType.FADE_OUT && 'ani-fade-out',
+            video.classNames
+          ])}
           autoPlay
           playsInline
           muted
@@ -35,14 +82,24 @@ const FlamingHome: FC = () => {
       ))
       }
 
-      <div>
+      <div
+        style={{
+          '--translate-x': '0',
+          '--translate-y': '0',
+          '--duration-time': `${aniTime}s`
+        } as CSSProperties}
+        className={cn([
+          aniSts === SliderAnimationType.FADE_IN && 'ani-fade-in',
+          aniSts === SliderAnimationType.FADE_OUT && 'ani-fade-out',
+        ])}
+      >
         {imgUrls.map((img, index) => {
           return (
             <S3Image key={index} src={img.url} className={cn(['absolute object-contain', img.classNames])} />
           )
         })}
       </div>
-    </div>
+    </div >
 
   )
 }
