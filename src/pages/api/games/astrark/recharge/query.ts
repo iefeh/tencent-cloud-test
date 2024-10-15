e9940946d57965aecd44002d6a303d65a3dd1fda
Frank <36853880+iefeh@users.noreply.github.com>
2024-10-15 13:41:52 +0800

    add rebate rate.
    add rebate rate.

--- src/pages/api/games/astrark/recharge/query.ts
+++ src/pages/api/games/astrark/recharge/query.ts
@@ -10,21 +10,31 @@ const router = createRouter<UserContextRequest, NextApiResponse>();
 
 router.use(mustAuthInterceptor).get(async (req, res) => {
     const userId = req.userId!;
-    const total = await queryTotalRecharge(userId);
-    res.json(response.success({ total: total }));
+    const [total, rebate] = await queryTotalRecharge(userId);
+    res.json(response.success({ total: total, reabte: rebate }));
 });
 
-export async function queryTotalRecharge(userId: string) {
+export async function queryTotalRecharge(userId: string): Promise<[number, number]> {
     const quest = await Quest.findOne({ id: 'astrark-recharge-query' });
     const queryQuest = new ThinkingDataQueryQuest(quest);
     const questProp = quest.properties as ThinkingDataQuery;
     const result = await queryQuest.checkUserQuestFromThinkingData(questProp, userId);
 
     if (!result) {
-        return 0;
+        return [0, 0];
+    }
+    const total = Number(Number(result[1][0]).toFixed(2));
+    let rebate: number;
+    
+    if (total <= 200) {
+        rebate = 1.5;
+    } else if (total <= 800) {
+        rebate = 1.2;
+    } else {
+        rebate = 1;
     }
 
-    return Number(Number(result[1][0]).toFixed(2));
+    return [total, rebate];
 }
 
 // this will run if none of the above matches
