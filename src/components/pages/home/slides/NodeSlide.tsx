e829bce74e01da6990b7cc91bcc15de114d55e04
Eki <ichinoseeki@outlook.com>
2024-10-23 19:19:04 +0800

    feat: countdown.

--- src/components/pages/home/slides/NodeSlide.tsx
+++ src/components/pages/home/slides/NodeSlide.tsx
@@ -1,7 +1,11 @@
 import S3Image from '@/components/common/medias/S3Image';
+import TokenRewardProgressCountdown from '@/components/common/task/TokenRewardProgressCountdown';
 import Link from '@/components/link';
+import useCountdown from '@/hooks/useLoopCountDown';
 import type { HomeSlide } from '@/types/lottery';
-import { FC } from 'react';
+import { cn } from '@nextui-org/react';
+import dayjs from 'dayjs';
+import { FC, Fragment, useState } from 'react';
 import { isMobile } from 'react-device-detect';
 
 interface Props {
@@ -9,9 +13,18 @@ interface Props {
 }
 
 const NodeSlide: FC & HomeSlide = (props: Props) => {
+  const [cdNumbers, setCDNumbers] = useState(Array(4).fill('00'));
+  useCountdown(+(process.env.NEXT_PUBLIC_PUCLIC_SALE_TIME || 0) || dayjs().valueOf(), dayjs().valueOf(), (time) => {
+    const du = dayjs.duration(time);
+    let hours: number | string = ~~du.asHours();
+    if (hours > 99) hours = '99+';
+    const nos = [hours, du.minutes(), du.seconds()].map((n) => n.toString().padStart(2, '0'));
+    setCDNumbers(nos);
+  });
+
   return (
     <div className="bg-entertainment w-full h-screen relative flex justify-center items-center p-12">
-      <Link href={process.env.NEXT_PUBLIC_URL_NODE_SALE!} target='_blank'>
+      <Link href={process.env.NEXT_PUBLIC_URL_NODE_SALE!} target="_blank">
         {isMobile ? (
           <S3Image className="object-cover" src="/home/bg_home_swiper_node_sale.png" fill />
         ) : (
@@ -28,6 +41,33 @@ const NodeSlide: FC & HomeSlide = (props: Props) => {
               type="video/webm"
             />
           </video>
+        )}
+
+        {process.env.NEXT_PUBLIC_PUCLIC_SALE_TIME && (
+          <div
+            className={cn([
+              'absolute h-[2.5rem] flex justify-center items-center gap-y-4',
+              isMobile ? 'left-1/2 -translate-x-1/2 top-[44%] flex-wrap' : 'right-48 bottom-32',
+            ])}
+          >
+            <span className="mr-2 text-3xl text-basic-yellow font-semibold drop-shadow whitespace-nowrap">
+              Public Sale starts in
+            </span>
+
+            {cdNumbers.map((no, index) => (
+              <Fragment key={index}>
+                {index > 0 && <span className="mx-1">:</span>}
+                <div
+                  className={cn([
+                    'px-1 bg-[#E0D1B1] rounded-five text-brown text-center font-semibold',
+                    'text-2xl h-full min-w-[2.5rem] leading-[2.5rem]',
+                  ])}
+                >
+                  {no}
+                </div>
+              </Fragment>
+            ))}
+          </div>
         )}
       </Link>
     </div>
