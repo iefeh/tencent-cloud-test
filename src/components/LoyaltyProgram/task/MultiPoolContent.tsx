121e7564e880ea8d77b7432ce74d0ea788e709f5
Eki <ichinoseeki@outlook.com>
2024-10-29 14:46:23 +0800

    feat: text.

--- src/components/LoyaltyProgram/task/MultiPoolContent.tsx
+++ src/components/LoyaltyProgram/task/MultiPoolContent.tsx
@@ -5,7 +5,7 @@ import { TaskCategory } from '@/http/services/battlepass';
 const MultiPoolContent: FC = () => {
   const [currentCategory, setCurrentCategory] = useState<TaskCategory>({
     id: 'multi418-449b-49c3-bf98-e56d37a648f5',
-    name: 'Multi Pool',
+    name: '50K Pool #2',
     quest_count: 0,
     achieve_count: 0,
     image_url: '',
--- src/components/LoyaltyProgram/task/PhasePoolContent.tsx
+++ src/components/LoyaltyProgram/task/PhasePoolContent.tsx
@@ -5,7 +5,7 @@ import { TaskCategory } from '@/http/services/battlepass';
 const PhasePoolContent: FC = () => {
   const [currentCategory, setCurrentCategory] = useState<TaskCategory>({
     id: 'phase9b3-d40b-41d5-872b-d26890188d1b',
-    name: 'Phase Pool',
+    name: '50K POOL #1',
     quest_count: 0,
     achieve_count: 0,
     image_url: '',
