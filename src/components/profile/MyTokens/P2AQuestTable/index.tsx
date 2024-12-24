d22ddae976bb7ece0c6b731cb25bffa17ed529d6
Eki <ichinoseeki@outlook.com>
2024-12-24 16:33:24 +0800

    feat: tab of p2a in My-Tokens.

--- src/components/profile/MyTokens/P2AQuestTable/index.tsx
+++ src/components/profile/MyTokens/P2AQuestTable/index.tsx
@@ -1,7 +1,7 @@
 import { Listbox, ListboxItem, Pagination, ScrollShadow } from '@nextui-org/react';
 import { FC } from 'react';
-import QuestTokenRow from './QuestTokenRow';
-import { QuestTokensRecord } from '@/http/services/token';
+import P2AQuestTokenRow from './P2AQuestTokenRow';
+import { P2AQuestTokensRecord } from '@/http/services/token';
 import Image from 'next/image';
 import teamsImg from 'img/loyalty/task/teams.png';
 import useClaimToken from '@/hooks/pages/profile/myTokens/useClaimToken';
@@ -11,28 +11,25 @@ import PaginationRenderItem from '@/components/LoyaltyProgram/task/PaginationRen
 interface Props {
   pagiTotal?: number;
   loading?: boolean;
-  items: QuestTokensRecord[];
+  items: P2AQuestTokensRecord[];
   onPagiChange?: (page: number) => void;
 }
 
-const QuestTable: FC<Props> = ({ pagiTotal = 1, loading, items, onPagiChange }) => {
+const P2AQuestTable: FC<Props> = ({ pagiTotal = 1, loading, items, onPagiChange }) => {
   const emptyContent = (
     <div className="relative backdrop-saturate-150 backdrop-blur-md bg-overlay/30 z-0 flex flex-col justify-center items-center font-poppins text-2xl pointer-events-none">
       <p>No history found.</p>
       <Image className="w-[54rem] h-auto" src={teamsImg} alt="" />
     </div>
   );
-  const { onClaim } = useClaimToken({ updateList: () => onPagiChange?.(1) });
 
   return (
     <>
       <ul className="w-full flex justify-between items-center h-16 bg-[#111111] text-[#999] px-0 md:px-10 gap-4">
-        <li className="flex-[360]">Token</li>
-        <li className="flex-[264]">Quantity</li>
-        <li className="flex-[224]">Network</li>
-        <li className="flex-[156]">Status</li>
-        <li className="flex-[156]">Claim Time</li>
-        <li className="w-16 md:w-40 shrink-0 text-right">View Tx</li>
+        <li className="flex-[360]">Item</li>
+        <li className="flex-[264]">Reward Type</li>
+        <li className="flex-[224]">Token</li>
+        <li className="flex-[156] text-right">Time</li>
       </ul>
 
       <ScrollShadow className="w-full min-h-[8rem] max-h-[31.375rem] font-poppins-medium relative">
@@ -41,9 +38,9 @@ const QuestTable: FC<Props> = ({ pagiTotal = 1, loading, items, onPagiChange }) 
             <ListboxItem
               key={Math.random().toString()}
               className="rounded-none p-0 !bg-transparent"
-              textValue={item.source_type}
+              textValue={item.source}
             >
-              <QuestTokenRow item={item} onClaim={onClaim} />
+              <P2AQuestTokenRow item={item} />
             </ListboxItem>
           )}
         </Listbox>
@@ -72,4 +69,4 @@ const QuestTable: FC<Props> = ({ pagiTotal = 1, loading, items, onPagiChange }) 
   );
 };
 
-export default QuestTable;
+export default P2AQuestTable;
