c5e0bbf33dd7f0d9bb240159fe565530eea3dbf9
Eki <ichinoseeki@outlook.com>
2025-01-24 10:57:52 +0800

    feat: compliance for lottery.

--- src/types/lottery.d.ts
+++ src/types/lottery.d.ts
@@ -5,6 +5,7 @@ import type { LotteryRequirementDTO } from '@/http/services/lottery';
 
 declare namespace Lottery {
   interface Pool<T = RewardItem> {
+    chain_id?: string;
     lottery_pool_id: string;
     start_time: number;
     end_time: number;
