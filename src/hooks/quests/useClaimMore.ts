d7eaab742aa0a7e0833334b8c5ef94d497f37e04
Eki <ichinoseeki@outlook.com>
2025-02-05 15:56:59 +0800

    feat: store ticket hash.

--- src/hooks/quests/useClaimMore.ts
+++ src/hooks/quests/useClaimMore.ts
@@ -3,12 +3,24 @@ import useTransaction from '../wallet/useTransaction';
 import gamePaymentABI from '@/http/abi/gamePayment.json';
 import { QuestType } from '@/constant/task';
 import { toast } from 'react-toastify';
+import Cookies from 'js-cookie';
+import { useUserContext } from '@/store/User';
 
 export default function useClaimMore(task: TaskListItem) {
   const canClaimMore = task.type === QuestType.ClaimFreeTicket;
+  const { userInfo } = useUserContext();
   const { onTransaction, getTransaction } = useTransaction({ abi: gamePaymentABI, method: 'claimFreeTicket' });
 
   const onGetFreeTicket = async () => {
+    if (!userInfo) return;
+
+    const hashKey = `${userInfo.user_id}:${task.id}`;
+    const hashValue = Cookies.get(hashKey);
+    if (hashValue) {
+      toast.error('You have already submitted the hash. Please wait for verification.');
+      return null;
+    }
+
     const res = await onTransaction({
       config: {
         chainId: task.properties.chain_id,
@@ -18,6 +30,7 @@ export default function useClaimMore(task: TaskListItem) {
     });
 
     const hash = res?.hash || '';
+    if (!hash) return null;
 
     try {
       const info = await getTransaction(hash);
@@ -27,6 +40,8 @@ export default function useClaimMore(task: TaskListItem) {
       toast.error('Network error, please try again later.');
       return null;
     }
+
+    Cookies.set(hashKey, hash, { expires: 300 / 86400 });
 
     return hash;
   };
