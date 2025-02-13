ff252c8cba50467702d8750c3be654d518689b47
Eki <ichinoseeki@outlook.com>
2025-02-13 15:19:46 +0800

    feat: replace url.

--- src/components/pages/home/slides/MinigamesSlide.tsx
+++ src/components/pages/home/slides/MinigamesSlide.tsx
@@ -19,7 +19,7 @@ export default function MinigamesSlide(props: Props) {
     >
       <Image
         className="object-cover"
-        src="https://d3dhz6pjw7pz9d.cloudfront.net/home/bg_home_swiper_minihub.png"
+        src="https://cdn.moonveil.gg/home/bg_home_swiper_minigames.png"
         alt=""
         fill
         sizes="100%"
