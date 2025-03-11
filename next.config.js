71cdf2ffd229e3528bf3cdf7444e0278734b3a71
Frank <36853880+iefeh@users.noreply.github.com>
2025-03-11 11:13:12 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -145,3 +145,11 @@ module.exports = async () => {
 
   return {};
 }
+
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
